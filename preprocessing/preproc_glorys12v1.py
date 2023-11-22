000b658589cb9e4e8df3fe0fee1da4fff2292941
Glenn Liu <glenn.y.liu@gmail.com>
2023-11-22 17:24:07 -0500

    Add variable loop

--- preprocessing/preproc_glorys12v1.py
+++ preprocessing/preproc_glorys12v1.py
@@ -51,55 +51,61 @@ outpath  = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/g
 years    = np.arange(1993,2020)
 mons     = np.arange(1,13,1)
 bbox     = [-100,20,-20,75]
-varname  = "so"
-keepvars = ["longitude","latitude","depth","time", varname]
+
 debug    = False
 
 #%% Main Script
 nyrs     = len(years)
+varnames = ["thetao","zos","uo","vo","mlotst",]
+nvars    = len(varnames)
 
-for y in range(nyrs):
+for v in range(nvars):
+    varname  = varnames[v]
+    keepvars = ["longitude","latitude","depth","time", varname]
     
-    year    = years[y]
-    montime = []
-    mon_ds  = []
-    for im in range(12): # Begin Month Loop
+    for y in range(nyrs):
         
-        # Get list of files for the month
-        mon   = mons[im]
-        flist = glob.glob("%s%s/mercatorglorys12v1_gl12_mean_%04i%02i*.nc" % (datpath,year,year,mon))
-        flist.sort()
-        ndays = len(flist)
+        year    = years[y]
+        montime = []
+        mon_ds  = []
+        for im in range(12): # Begin Month Loop
+            
+            # Get list of files for the month
+            mon   = mons[im]
+            flist = glob.glob("%s%s/mercatorglorys12v1_gl12_mean_%04i%02i*.nc" % (datpath,year,year,mon))
+            flist.sort()
+            ndays = len(flist)
+            
+            # Open and grab daily data at a given level/region
+            daily_ds = []
+            for d in tqdm.tqdm(range(ndays)): # Begin Daily Loop
+                st         = time.time()
+                ds         = xr.open_dataset(flist[d])      # Open NetCDF View
+                ds         = crop_ds(ds,keepvars)
+                ds         = ds.load()
+                if debug:
+                    print("Loaded in %.2fs" % (time.time()-st))
+                daily_ds.append(ds)
+                # <End Daily Loop> --------
+            
+            # Concatenate daily data and take mean over montyh
+            daily_ds = xr.concat(daily_ds,dim='time')
+            monavg   = daily_ds.mean('time')
+            montime.append(daily_ds.isel(time=0).time.values) # Append first value of the month
+            mon_ds.append(monavg)
+            print("Completed merging for year %04i, Month %02i..." % (year,mon))
+            # <End Month Loop> --------
         
-        # Open and grab daily data at a given level/region
-        daily_ds = []
-        for d in tqdm.tqdm(range(ndays)): # Begin Daily Loop
-            st         = time.time()
-            ds         = xr.open_dataset(flist[d])      # Open NetCDF View
-            ds         = crop_ds(ds,keepvars)
-            ds         = ds.load()
-            if debug:
-                print("Loaded in %.2fs" % (time.time()-st))
-            daily_ds.append(ds)
-            # <End Daily Loop> --------
-        
-        # Concatenate daily data and take mean over montyh
-        daily_ds = xr.concat(daily_ds,dim='time')
-        monavg   = daily_ds.mean('time')
-        montime.append(daily_ds.isel(time=0).time.values) # Append first value of the month
-        mon_ds.append(monavg)
-        print("Completed merging for year %04i, Month %02i..." % (year,mon))
-        # <End Month Loop> --------
-    
-    # Save files
-    stsv     = time.time()
-    mon_ds   = xr.concat(mon_ds,dim="time") # Concatenate Files
-    mon_ds   =  mon_ds.assign_coords({'time':montime}) # Add Time dimension
-    savename = "%sglorys12v1_%s_NAtl_%04i.nc" % (outpath,varname,year)
-    edict    = proc.make_encoding_dict(mon_ds)
-    mon_ds.to_netcdf(savename,encoding=edict)
-    print("Saved file for year %04i in %.2fs" % (year,time.time()-stsv))
-    # <End Year Loop> --------
+        # Save files
+        stsv     = time.time()
+        mon_ds   = xr.concat(mon_ds,dim="time") # Concatenate Files
+        mon_ds   =  mon_ds.assign_coords({'time':montime}) # Add Time dimension
+        savename = "%sglorys12v1_%s_NAtl_%04i.nc" % (outpath,varname,year)
+        edict    = proc.make_encoding_dict(mon_ds)
+        mon_ds.to_netcdf(savename,encoding=edict)
+        print("Saved file for year %04i in %.2fs" % (year,time.time()-stsv))
+        # <End Year Loop> --------
+    # End variable loop
 
 
 
