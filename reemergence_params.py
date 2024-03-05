7009acd87b1bf39d2bd2d633e5fdcf931868f9a2
Glenn Liu <glenn.y.liu@gmail.com>
2024-03-05 16:11:20 -0500

    Add procpath to rparams

--- reemergence_params.py
+++ reemergence_params.py
@@ -28,6 +28,7 @@ mdict0 = {
     "raw_path"          : 0,        # Path to post-processed CESM1 Output
     "input_path"        : 0,        # Path to stochastic model input (forcing, damping, mld)
     "output_path"       : 0,        # Path to stochastic model output
+    "procpath"          : 0,        # Path to analysis output/processed data
     "amvpath"           : 0,        # Path to analysis tools module...
     "scmpath"           : 0,        # Path to stochastic model module
     "figpath"           : 0,        # Path to figure output
@@ -37,12 +38,13 @@ mdict0 = {
 # Astraeus Local ---
 mdict0 = {
     "machine"           : "Astraeus",     # Name of the machine
-    "raw_path"          : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/CESM1/NATL_proc/", # Path to pre-processed CESM1 Output, cropped to NATL
-    "input_path"        : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/model_input/",        # Path to stochastic model input (forcing, damping, mld)
-    "output_path"       : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/sm_experiments/",        # Path to stochastic model output
-    "amvpath"           : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/",        # Path to analysis tools module...
-    "scmpath"           : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/",        # Path to stochastic model module
-    "figpath"           : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/02_Figures/%s/"  % outdate,# Path to figure output
+    "raw_path"          : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/CESM1/NATL_proc/",      # Path to pre-processed CESM1 Output, cropped to NATL
+    "input_path"        : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/model_input/",          # Path to stochastic model input (forcing, damping, mld)
+    "output_path"       : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/sm_experiments/",            # Path to stochastic model output
+    "procpath"          : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/",                       # Path to analysis output/processed data
+    "amvpath"           : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/",                            # Path to analysis tools module...
+    "scmpath"           : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/",            # Path to stochastic model module
+    "figpath"           : "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/02_Figures/%s/"  % outdate,          # Path to figure output
     }
 
 # Stormtrack Server ---
@@ -51,6 +53,7 @@ mdict1 = {
     "raw_path"          : "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/CESM1/NATL_proc/",    # Path to post-processed CESM1 Output
     "input_path"        : "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/model_input/",        # Path to stochastic model input (forcing, damping, mld)
     "output_path"       : "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/sm_experiments/",          # Path to stochastic model output
+    "procpath"          : "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/",                    # Path to analysis output/processed data
     "amvpath"           : "/home/glliu/00_Scripts/01_Projects/00_Commons/",                                         # Path to analysis tools module...
     "scmpath"           : "/home/glliu/00_Scripts/01_Projects/01_AMV/02_stochmod/stochmod/model/",                  # Path to stochastic model module
     "figpath"           : "/home/glliu/02_Figures/01_WeeklyMeetings/%s/"  % outdate,                                # Path to figure output
@@ -60,7 +63,6 @@ mdict1 = {
 machine_path_dicts  = (mdict0,mdict1,)
 machine_names       = [d["machine"] for d in machine_path_dicts]
 machine_paths       = dict(zip(machine_names,machine_path_dicts))
-
 
 #%%
 
