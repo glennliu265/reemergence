3a957b152a5657162712eb641c6e6f3869cad299
Glenn Liu <glenn.y.liu@gmail.com>
2024-03-06 17:41:58 -0500

    Deleted old code sections

--- stochastic_model/run_SSS_basinwide.py
+++ stochastic_model/run_SSS_basinwide.py
@@ -137,22 +137,6 @@ L     = 2.5e6      # Specific Heat of Evaporation [J/kg], from SSS model documen
 
 debug = False
 
-#%% Load Params
-
-# Parameters to check for []
-#chk_params = ["mld","evap_forcing","precip_forcing","Sbar","lbd_d","beta","kprev","lbd_a"]
-
-
-# ds_mld   = xr.open_dataset(input_path + "mld/" + expparams['mld'])['h']#.h.values
-# ds_e     = xr.open_dataset(input_path + "forcing/" + expparams['evap_forcing'])['LHFLX']
-# ds_p     = xr.open_dataset(input_path + "forcing/" + expparams['precip_forcing'])['PRECTOT']
-# ds_sbar  = xr.open_dataset(input_path + "forcing/" + expparams['Sbar'])['Sbar']
-
-# if type(expparams['lbd_d'])==str:
-#     ds_lbdd   = xr.open_dataset(input_path + "damping/" + expparams['lbd_d'])['lbd_d']#.h.values
-# else: # Assuming Td is a single value...
-#     ds_lbdd = xr.ones_like(ds_p).rename("lbd_d") * expparams['lbd_d']
-
 #%% Check and Load Params
 
 print("Loading inputs for %s" % expname)
@@ -227,15 +211,6 @@ for nn in range(ninputs):
         print("Did not find %s" % pname)
         missing_input.append(pname)
     inputs_type[pname] = ptype
-
-    # elif type(expparams[pname])==float:
-    #     # Make empty data_array, multiplied by the given value
-    #     print("For <%s> Making Empty DataArray with the repeated value %f" % (pname,expparams[pname]))
-    #     ds = xr.ones_like(inputs[nn-1]).rename(pname) * expparams[pname]
-    # else:
-    #     missing_input.append(pname)
-# Crop to Region
-#varcrop     = [proc.sel_region_xr(ds,expparams['bbox_sim']).load().values for ds in inputs] 
 
 #%% Detect and Process Missing Inputs
 
@@ -441,19 +416,9 @@ for nr in range(nruns):
         
         # Tile Forcing (need to move time dimension to the back)
         if eof_flag: # Append Qfactor as an extra mode
-            # alpha_tile=[]
-            # for ii in tqdm.tqdm(range(nmode)): # Tile for each mode
-            #     inalphatile = np.tile(alpha[ii,:,:,:].transpose(1,2,0),expparams['nyrs']).transpose(2,0,1) 
-                
-            #     alpha_tile.append(inalphatile)
-            # corr_tile  = np.tile(Qfactor.transpose(1,2,0),expparams['nyrs']).transpose(2,0,1) 
-            # alpha_tile = np.array(alpha_tile) # Note this takes forever....
-            # # Combine them This also takes forver
-            # alpha_tile = np.concatenate([alpha_tile,corr_tile[None,...]],axis=0)
+
             alpha = np.concatenate([alpha,Qfactor[None,...]],axis=0)
-        #else:
-            #alpha_tile = np.tile((alpha).transpose(1,2,0),expparams['nyrs']).transpose(2,0,1) # [Time x Lat x Lon]
-        
+            
         # Calculate beta and kprev
         beta       = scm.calc_beta(inputs['h'].transpose(2,1,0)) # {lon x lat x time}
         if expparams['kprev'] is None: # Compute Kprev if it is not supplied
@@ -522,176 +487,5 @@ for nr in range(nruns):
         edict    = {expparams['varname']:{"zlib":True}}
         savename = "%sOutput/%s_runid%s.nc" % (expdir,expparams['varname'],runid)
         da.to_netcdf(savename,encoding=edict)
-    
 
-
-
-#%%
-
-
-# def ssr(v,dim):
-#     return np.sqrt(np.sum(v**2,dim))
-
-
-
-# Mean Stdev (in time) of all points
-# Qekconvert: 0.008140762034669971
-# Econvert  : 0.004727825910126515
-# Pconvert  : 0.009327264991185927
-# alpha     : 0.016826414463990486
-# forcing_in : 1.0124996310252043
-
-
-# # #%%  Debugging for above
-
-
-# # plt.plot(e[:,klat,klon]),plt.show()
-
-# # plt.plot(Econv[:,klat,klon]),plt.show()
-# # plt.plot(EP_forcing[:,klat,klon]),plt.show()
-
-
-# # #%% Crop to region
-
-
-# # xr.cftime_range(start="0001",periods=SSS_out.shape[-1],freq="MS",calendar="noleap")
-
-
-# # #%% Load Inputs
-
-
-
-# # #%%
-
-
-
-
-
-# # exp_params = {
-
-# #     }
-
-# #%% PCompare Point Output -----------
-# mons3 = proc.get_monstr(nletters=3)
-# locfn,loctitle=proc.make_locstring(330,50)
-
-# fig,axs = plt.subplots(3,1,constrained_layout=True,figsize=(6,8))
-
-
-# # Plot MLD
-# ax = axs.flatten()[0]
-# ax = viz.viz_kprev(inputs['h'][:,klat,klon],inputs['kprev'][:,klat,klon],ax=ax,usetitle=False,lw=2.5)
-# #ax.plot(mons3,inputs['h'][:,klat,klon],label="MLD",marker="o",lw=3.5)
-# ax.set_ylabel("MLD (meters)")
-# ax.legend()
-
-# # Plot Beta
-# ax = axs.flatten()[1]
-# ax.plot(mons3,beta[klon,klat,:],label="beta",marker="o",lw=3.5,color="darkblue")
-# ax.plot(mons3,inputs['lbd_d'][:,klat,klon],label="$\lambda^d$",marker="d",lw=3.5,color="limegreen")
-# ax.plot(mons3,inputs['lbd_a'][:,klat,klon],label="$\lambda^a$",marker="d",lw=3.5,color="violet")
-# ax.set_ylabel("Damping (1/mon)")
-# ax.legend()
-
-# ax = axs.flatten()[2]
-# ax.plot(mons3,Econvert[:,klat,klon],label="E'",marker="o",lw=3.5,color="orange")
-# ax.plot(mons3,Pconvert[:,klat,klon],label="P'",marker="o",lw=3.5,color="b")
-# ax.set_ylabel("Forcing (psu/mon)")
-# ax.legend()
-
-# plt.suptitle("SSS Input Parameters @ %s" % loctitle)
-# for ax in axs:
-#     ax.grid(True,ls='dotted')
-
-# plt.show()
-
-# # 
-# #%% Examine/compare for point output
-
-# # Load in CESM Output
-# ssspath= "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/CESM1/NATL_proc/"
-# sssnc  = "CESM1LE_SSS_NAtl_19200101_20050101_bilinear.nc"
-# dsc    = xr.open_dataset(ssspath+sssnc)
-# sss_cesm = dsc.sel(lon=-30,lat=50,method='nearest').SSS.load()#.values
-
-# # Deseason, Detrend
-# sss_anom = proc.xrdeseason(sss_cesm)
-# sss_dt   = sss_anom - sss_anom.mean('ensemble')
-# sss_cesm = sss_dt.values
-
-# sss_cesm[np.isnan(sss_cesm)] = 0
-
-# #%% Compute Metrics
-
-# sss_pt = SSS_out.squeeze()
-# tsm    = scm.compute_sm_metrics([sss_pt,sss_cesm.flatten()],)
-
-
-# #%% PLot ACF
-# kmonth = 1
-# lags = np.arange(37)
-# xtks = np.arange(0,38,3)
-
-# fig,ax = plt.subplots(1,1,constrained_layout=True)
-
-# ax,_=viz.init_acplot(kmonth,xtks,lags)
-# ax.plot(lags,tsm['acfs'][kmonth][0],label="SM")
-# ax.plot(lags,tsm['acfs'][kmonth][1],label="CESM")
-# ax.legend()
-# plt.show()
-
-# #%% Plot Monvar
-
-# fig,ax = viz.init_monplot(1,1)
-# ax.plot(mons3,tsm['monvars'][0],label="SM")
-# ax.plot(mons3,tsm['monvars'][1],label="CESM")
-# ax.set_title("Monthly Variance")
-# ax.set_ylabel("SST Variance ($\degree C^2$)")
-# ax.legend()
-# plt.show()
-
-# #%% Plot Timseries
-
-# fig,axs = plt.subplots(2,1,constrained_layout=True,figsize=(12,4))
-# ax =axs[0]
-# ax.plot(sss_pt[1800:],label="SM")
-# ax.set_title("SM")
-# ax = axs[1]
-# ax.set_ylim([1800,1824])
-
-# for e in range(42):
-#     ax.plot(sss_cesm[e,:],label="",color='orange',alpha=0.2)
-# ax.set_title("CESM")
-# ax.legend()
-# plt.show()
-
-# #%%p
-
-# plt.plot(EP_forcing[:,klat,klon])
-# ax.set_ylim([1800,1824])
-# plt.show()
-
-
-# #%%
-
-# plt.plot(tsm['acfs'][1][0]),plt.show()
-# plt.plot(tsm['monvars'][0]),plt.show()
-
-
-# #%% Briefly examine output
-
-# sss_pt = SSS_out[klon,klat]
-# tsm = scm.compute_sm_metrics([sss_pt,],)
-
-
-# plt.plot(SSS_out.squeeze()),plt.show()
-
-# plt.plot(Econvert[:,klat,klon]),plt.show()
-# plt.plot(Pconvert[:,klat,klon]),plt.show()
-
-
-
-# plt.plot(inputs['h'][:,klat,klon]),plt.show()
-# plt.plot(inputs['lbd_d'][:,klat,klon]),plt.show()
-# plt.plot(beta[klon,klat,:]),plt.show()
 
