5920dfe2e0c24b073b1dcb72cec7cb129a2f3da1
Glenn Liu <glenn.y.liu@gmail.com>
2024-05-20 15:53:49 -0400

    Just save output already computed by xr.func

--- detrainment_damping/calc_detrainment_depth.py
+++ detrainment_damping/calc_detrainment_depth.py
@@ -160,170 +160,120 @@ if dtdepth:
         )
     print("Completed hdetrain calc in %.2fs" % (time.time()-st))
 
-# Loop for Variable
-# Loop for Ens
+#%% Save the output
 
-#for vv in range(len(vnames)):
+outname = "%sCESM1_HTR_FULL_hdetrain_NAtl.nc" % (mldpath)
+hdetrainall.to_netcdf(outname)
 
-#ie = 0
-#vv = 0
+outname = "%sCESM1_HTR_FULL_kprev_NAtl.nc" % (mldpath)
+kprevall.to_netcdf(outname)
 
-for vv in range(len(vnames)):
-    vname = vnames[vv]
+
+# Note, the above section was moved back to the original script
+
+
+#%%
+
+# #%%
+# # Loop for Variable
+# # Loop for Ens
+
+# #for vv in range(len(vnames)):
+
+# #ie = 0
+# #vv = 0
+
+# vv = 0
+# for ie in range(len(loopens)):
+#     e     = loopens[ie]
     
-    for ie in range(len(loopens)):
-        e     = loopens[ie]
-        
-        
-        # Load the Data
-        st=time.time()
-        fn      = "%sCESM1_HTR_FULL_lbd_d_params_%s_detrend%s_lagmax%i_ens%02i_regridNN.nc" % (outpath,vname,detrend,lagmax,e+1)
-        ds      = xr.open_dataset(fn)
-        lon     = ds.lon
-        lat     = ds.lat
-        z_t     = ds.z_t
-        acf_ens = ds.acf_mon.load()
-        print("Loaded dataset for ens %02i in %.2fs" % (e+1,time.time()-st))
-        
-        
-        
-        corr_out    = np.zeros(ds.lbd_d.shape)*np.nan
-        _,nlat,nlon = corr_out.shape
-        
-        # Loop for each point
-        for a in tqdm(range(nlat)):
-            for o in range(nlon):
-                
-                # Select MLD at point
-                if dtdepth:
-                    hpt    = dsmld.sel(lon=lon[o],lat=lat[a],method='nearest').isel(ens=e)
-                else:
-                    hpt    = dsmld.isel(ens=e,lat=a,lon=o) # [Mon,]
-                if np.all(np.isnan(hpt)): # Skip for land point
-                    continue
-                
-                immax = hpt.argmax().values.item()
-                immin = hpt.argmin().values.item()
-                
-                # Compute kprev for the ensemble member
-                if dtdepth:
-                    kprev   = kprevall.sel(lon=lon[o],lat=lat[a],method='nearest').isel(ens=e).values
-                else:
-                    kprev,_ = scm.find_kprev(hpt)
-                
-                for im in range(12): # Loop for entrain month
-
-                    
-                    # Get the detrain month
-                    detrain_mon = kprev[im]
-                    if detrain_mon == 0.:
-                        continue # Skip when there is no entrainment
-                    
-                    # Get indices for autocorrelation
-                    dtid_floor = int(np.floor(detrain_mon)) - 1 
-                    dtid_ceil  = int(np.ceil(detrain_mon)) - 1
-                    entrid     = (im - imshift)%12
-                    # Check for cases (on first detrain month) where dt_ceil > entr_id
-                    if (dtid_ceil) >= entrid:
-                        
-                        if (dtid_floor) >= entrid: # Entrain Month # lower than detraining months, so it is one year later. Add 12
-                            entrid = entrid + 12
-                            #if debug:
-                                # print("dtfloor: %i, dtceil: %i,  entrid %i" % (dtid_floor,dtid_ceil,entrid))
-                                # print("WARNING: Floor of detrain month >= the entraining month. Check o=%i,a=%i, mon %02i, ens %02i for %s" % (o,a,im+1,e+1,vname))
-                                
-                            
-                        else: # first Entraining month, set to same value...
-                            # if debug:
-                            #     print("dtfloor: %i, dtceil: %i,  entrid %i" % (dtid_floor,dtid_ceil,entrid))
-                            #     print("Settin ceil to floor")
-                            dtid_ceil = dtid_floor # Just double count the same value
-                        
-                        #break
-                        #quit()
-                    if debug:
-                        print("Detaining Months [%i,%f,%i], Entrain Month [%i]" % (dtid_floor+1,detrain_mon,dtid_ceil+1,entrid+1))
-                    
-                    # First, get the depths
-                    if dtdepth: # Just retrieve at the detrainment depth
-                    
-                        h_detrain  = hdetrainall.sel(lon=lon[o],lat=lat[a],method='nearest').isel(ens=e,mon=im).values.item(0)
-                        zz_floor   = proc.get_nearest(h_detrain,z_t.values)
-                        zz_ceil    = zz_floor # same depth for detrain
-                        
-                    else: # Retrieve ACF at each corresponding depth before/after the detrainment time
-                        
-                        h_floor    = hpt.isel(mon=dtid_floor).values.item()
-                        h_ceil     = hpt.isel(mon=dtid_ceil).values.item()
-                        zz_floor   = proc.get_nearest(h_floor,z_t.values)
-                        zz_ceil    = proc.get_nearest(h_ceil,z_t.values)
-                        
-                    # Retrieve the ACF, with lag 0 at the detrain month
-                    acf_floor = acf_ens.isel(lat=a,lon=o,mon=dtid_floor,z_t=zz_floor)     # [Lag]
-                    acf_ceil  = acf_ens.isel(lat=a,lon=o,mon=dtid_ceil,z_t=zz_ceil)       # [Lag]
-                    
-                    # Calculate dlag
-                    dlag_floor = entrid - dtid_floor
-                    if dlag_floor < 1:
-                        dlag_floor = dlag_floor + 12
-                    dlag_ceil  = entrid - dtid_ceil
-                    if dlag_ceil < 1:
-                        dlag_ceil  = dlag_ceil + 12
-                    
-                    # Retrieve Correlation
-                    corr_floor = acf_floor.isel(lag=dlag_floor).values.item()
-                    corr_ceil  = acf_ceil.isel(lag=dlag_ceil).values.item()
-                    
-                    # Interp if option is chosen
-                    if interpcorr:
-                        dm       = detrain_mon - (dtid_floor+1)
-                        corr_mon = np.interp(dm,[0,1],[corr_floor,corr_ceil])
-                        #corr_mon = np.interp(detrain_mon,[dtid_floor+1,dtid_ceil+1],[corr_floor,corr_ceil])
-                    elif detrainceil:
-                        corr_mon = corr_ceil
-                    else:
-                        
-                        corr_mon = corr_floor
-                    corr_out[im,a,o] = corr_mon.copy()
-                    
-        # Save the output (for a variable and ensemble member)
-        coords = dict(mon=np.arange(1,13,1),lat=lat,lon=lon)
-        da_out = xr.DataArray(corr_out,coords=coords,dims=coords,name="lbd_d")
-        edict  = {'lbd_d':{'zlib':True}}
-        savename = "%sCESM1_HTR_FULL_corr_d_%s_detrend%s_lagmax%i_interp%i_ceil%i_imshift%i_dtdepth%i_ens%02i_regridNN.nc" % (outpath,vname,detrend,lagmax,
-                                                                                                          interpcorr,detrainceil,imshift,dtdepth,e+1)
-        
-        da_out.to_netcdf(savename,encoding=edict)
-# Loop for Month
-# for im in range(12):
-#im = 0
-
-# Get Entrain Month
-# Get Detrain Month
-# Apply Floor/Ceil, Corrections
-# 
-
-#%% Combine files for each ensemble member
-
-ensmerge = np.arange(42)
-nens     = len(ensmerge)
-
-for vv in range(len(vnames)):
-    vname = vnames[vv]
     
-    dsall = []
-    for e in tqdm(range(nens)):
+#     # Load the Data
+#     st=time.time()
+#     fn      = "%sCESM1_HTR_FULL_lbd_d_params_%s_detrend%s_lagmax%i_ens%02i_regridNN.nc" % (outpath,vname,detrend,lagmax,e+1)
+#     ds      = xr.open_dataset(fn)
+#     lon     = ds.lon
+#     lat     = ds.lat
+#     z_t     = ds.z_t
+#     print("Loaded dataset for ens %02i in %.2fs" % (e+1,time.time()-st))
+    
+    
+    
+#     hdetrain_all    = np.zeros(ds.lbd_d.shape)*np.nan
+#     nlon=len(lon)
+#     nlat=len(lat)
+    
+#     # Loop for each point
+#     for a in tqdm(range(nlat)):
+#         for o in range(nlon):
+            
+#             # Select MLD at point
+#             if dtdepth:
+#                 hpt    = dsmld.sel(lon=lon[o],lat=lat[a],method='nearest').isel(ens=e)
+#             else:
+#                 hpt    = dsmld.isel(ens=e,lat=a,lon=o) # [Mon,]
+#             if np.all(np.isnan(hpt)): # Skip for land point
+#                 continue
+            
+#             immax = hpt.argmax().values.item()
+#             immin = hpt.argmin().values.item()
+            
+#             # Compute kprev for the ensemble member
+#             if dtdepth:
+#                 kprev   = kprevall.sel(lon=lon[o],lat=lat[a],method='nearest').isel(ens=e).values
+#             else:
+#                 kprev,_ = scm.find_kprev(hpt)
+            
+#             for im in range(12): # Loop for entrain month
+
+                
+#                 # Get the detrain month
+#                 detrain_mon = kprev[im]
+#                 if detrain_mon == 0.:
+#                     continue # Skip when there is no entrainment
+                
+#                 h_detrain  = hdetrainall.sel(lon=lon[o],lat=lat[a],method='nearest').isel(ens=e,mon=im).values.item(0)
+                
+#                 hdetrain_all[im,a,o] = h_detrain
+                
+#     # Save the output (for a variable and ensemble member)
+#     coords = dict(mon=np.arange(1,13,1),lat=lat,lon=lon)
+#     #da_out = xr.DataArray(hdetrain_all,coords=coords,dims=coords,name="lbd_d")
+#     edict  = {'lbd_d':{'zlib':True}}
+#     savename = "%sCESM1_HTR_FULL_corr_d_%s_detrend%s_lagmax%i_interp%i_ceil%i_imshift%i_dtdepth%i_ens%02i_regridNN.nc" % (outpath,vname,detrend,lagmax,
+#                                                                                                       interpcorr,detrainceil,imshift,dtdepth,e+1)
+    
+#     da_out.to_netcdf(savename,encoding=edict)
+# # Loop for Month
+# # for im in range(12):
+# #im = 0
+
+# # Get Entrain Month
+# # Get Detrain Month
+# # Apply Floor/Ceil, Corrections
+# # 
+
+# #%% Combine files for each ensemble member
+
+# ensmerge = np.arange(42)
+# nens     = len(ensmerge)
+
+# for vv in range(len(vnames)):
+#     vname = vnames[vv]
+    
+#     dsall = []
+#     for e in tqdm(range(nens)):
         
-        savename = "%sCESM1_HTR_FULL_corr_d_%s_detrend%s_lagmax%i_interp%i_ceil%i_imshift%i_dtdepth%i_ens%02i_regridNN.nc" % (outpath,vname,detrend,lagmax,                     
-                                                                                         interpcorr,detrainceil,imshift,dtdepth,e+1)
-        ds = xr.open_dataset(savename).load()
-        dsall.append(ds)
+#         savename = "%sCESM1_HTR_FULL_corr_d_%s_detrend%s_lagmax%i_interp%i_ceil%i_imshift%i_dtdepth%i_ens%02i_regridNN.nc" % (outpath,vname,detrend,lagmax,                     
+#                                                                                          interpcorr,detrainceil,imshift,dtdepth,e+1)
+#         ds = xr.open_dataset(savename).load()
+#         dsall.append(ds)
         
-    dsall = xr.concat(dsall,dim='ens')
-    savename2 = "%sCESM1_HTR_FULL_corr_d_%s_detrend%s_lagmax%i_interp%i_ceil%i_imshift%i_dtdepth%i_ensALL_regridNN.nc" % (outpath,vname,detrend,lagmax,                     
-                                                                                  interpcorr,detrainceil,imshift,dtdepth)
-    edict = {'lbd_d':{'zlib':True}}
-    dsall.to_netcdf(savename2,encoding=edict)
+#     dsall = xr.concat(dsall,dim='ens')
+#     savename2 = "%sCESM1_HTR_FULL_corr_d_%s_detrend%s_lagmax%i_interp%i_ceil%i_imshift%i_dtdepth%i_ensALL_regridNN.nc" % (outpath,vname,detrend,lagmax,                     
+#                                                                                   interpcorr,detrainceil,imshift,dtdepth)
+#     edict = {'lbd_d':{'zlib':True}}
+#     dsall.to_netcdf(savename2,encoding=edict)
     
     
                          
