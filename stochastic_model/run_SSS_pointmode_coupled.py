f3c8aaed50aba2346f775b5e0926fa9e0984b4e2
Glenn Liu <glenn.y.liu@gmail.com>
2024-05-28 15:26:42 -0400

    Save before edits
    Save before modifications to support greater modification of SST and SSS

--- stochastic_model/run_SSS_pointmode_coupled.py
+++ stochastic_model/run_SSS_pointmode_coupled.py
@@ -50,7 +50,7 @@ machine = "Astraeus"
 
 # First Load the Parameter File
 cwd = os.getcwd()
-sys.path.append("../")
+sys.path.append(cwd+"/..")
 import reemergence_params as rparams
 
 # Paths and Load Modules
@@ -78,23 +78,17 @@ import scm
 #%% 
 
 """
-SSS_EOF_Qek_Pilot
-
-Note: The original run (2/14) had the incorrect Ekman Forcing and used ens01 detrainment damping with linear detrend
-I reran this after fixing these issues (2/29)
-
+LHFLX Run (SST_SSS  Coupled, from early may prior to 2024.05.07)
 """
 
 # Paths and Experiment
-
-
-expname     = "SST_SSS_LHFLX" # Borrowed from "SST_EOF_LbddCorr_Rerun"
+expname     = "SST_SSS_TdcorrFalse" # Borrowed from "SST_EOF_LbddCorr_Rerun"
 expparams_sst   = {
     'varname'           : "SST",
     'bbox_sim'          : [-80,0,20,65],
     'nyrs'              : 1000,
     'runids'            : ["run%02i" % i for i in np.arange(0,10,1)],
-    'runid_path'        : None, # If not None, load a runid from another directory
+    'runid_path'        : "SST_SSS_LHFLX", # If not None, load a runid from another directory
     'Fprime'            : "CESM1_HTR_FULL_Eprime_EOF_nomasklag1_nroll0_NAtl_corrected_EnsAvg.nc",
     'PRECTOT'           : None,
     'LHFLX'             : None,
@@ -103,7 +97,7 @@ expparams_sst   = {
     'Sbar'              : None,
     'beta'              : None, # If None, just compute entrainment damping
     'kprev'             : "CESM1_HTR_FULL_kprev_NAtl_EnsAvg.nc",
-    'lbd_a'             : "CESM1_HTR_FULL_LHFLX_damping_nomasklag1_EnsAvg.nc", # Only latent heat flux
+    'lbd_a'             : "CESM1_HTR_FULL_LHFLX_damping_nomasklag1_EnsAvg.nc", # Only latent heat flux # "CESM1_HTR_FULL_qnet_damping_nomasklag1_EnsAvg.nc",#
     'Qek'               : None, # No Qekman #Must be in W/m2
     'convert_Fprime'    : True,
     'convert_lbd_a'     : True, # ALERT!! Need to rerun with this set to true....
@@ -115,7 +109,7 @@ expparams_sst   = {
     'halfmode'          : False,
     "entrain"           : True,
     "eof_forcing"       : True,
-    "Td_corr"           : True, # Set to True if lbd_d is provided as a correlation, rather than 1/months
+    "Td_corr"           : False, # Set to True if lbd_d is provided as a correlation, rather than 1/months
     "lbd_e"             : None,
     "Tforce"            : None,
     }
@@ -124,13 +118,13 @@ expparams_sss   = {
     'varname'           : "SSS",
     'bbox_sim'          : [-80,0,20,65],
     'nyrs'              : 1000,
-    'runids'            : ["run%02i" % i for i in np.arange(5,10,1)],
+    'runids'            : ["run%02i" % i for i in np.arange(0,10,1)],
     'runid_path'        : "SST_SSS_LHFLX",#"SST_EOF_Qek_pilot", # If not None, load a runid from another directory
     'Fprime'            : None,
     'PRECTOT'           : None, # No Precip
     'LHFLX'             : "CESM1_HTR_FULL_Eprime_EOF_nomasklag1_nroll0_NAtl_corrected_EnsAvg.nc",
     'h'                 : "CESM1_HTR_FULL_HMXL_NAtl_EnsAvg.nc",
-    'lbd_d'             : "CESM1_HTR_FULL_corr_d_TEMP_detrendensmean_lagmax3_interp1_imshift1_dtdepth1_EnsAvg.nc",
+    'lbd_d'             : "CESM1_HTR_FULL_corr_d_SALT_detrendensmean_lagmax3_interp1_imshift1_dtdepth1_EnsAvg.nc",
     'Sbar'              : "CESM1_HTR_FULL_Sbar_NAtl_EnsAvg.nc",
     'beta'              : None, # If None, just compute entrainment damping
     'kprev'             : "CESM1_HTR_FULL_kprev_NAtl_EnsAvg.nc",
@@ -146,10 +140,81 @@ expparams_sss   = {
     'halfmode'          : False,
     "entrain"           : True,
     "eof_forcing"       : True,
-    "Td_corr"           : True, # Set to True if lbd_d is provided as a correlation, rather than 1/months
+    "Td_corr"           : False, # Set to True if lbd_d is provided as a correlation, rather than 1/months
     "lbd_e"             : "CESM1LE_HTR_FULL_lbde_Bcorr3_lbda_qnet_damping_nomasklag1_EnsAvg.nc",
     "Tforce"            : "SST_SSS_LHFLX",
     }
+
+# """
+# Another Version, but without lbd_d
+
+# """
+
+
+# expname     = "SST_SSS_LHFLX_NoLbdd" # Borrowed from "SST_EOF_LbddCorr_Rerun"
+# expparams_sst   = {
+#     'varname'           : "SST",
+#     'bbox_sim'          : [-80,0,20,65],
+#     'nyrs'              : 1000,
+#     'runids'            : ["run%02i" % i for i in np.arange(0,10,1)],
+#     'runid_path'        : None, # If not None, load a runid from another directory
+#     'Fprime'            : "CESM1_HTR_FULL_Eprime_EOF_nomasklag1_nroll0_NAtl_corrected_EnsAvg.nc",
+#     'PRECTOT'           : None,
+#     'LHFLX'             : None,
+#     'h'                 : "CESM1_HTR_FULL_HMXL_NAtl_EnsAvg.nc",
+#     'lbd_d'             : False,
+#     'Sbar'              : None,
+#     'beta'              : None, # If None, just compute entrainment damping
+#     'kprev'             : "CESM1_HTR_FULL_kprev_NAtl_EnsAvg.nc",
+#     'lbd_a'             : "CESM1_HTR_FULL_LHFLX_damping_nomasklag1_EnsAvg.nc", # Only latent heat flux
+#     'Qek'               : None, # No Qekman #Must be in W/m2
+#     'convert_Fprime'    : True,
+#     'convert_lbd_a'     : True, # ALERT!! Need to rerun with this set to true....
+#     'convert_PRECTOT'   : False,
+#     'convert_LHFLX'     : False,
+#     'froll'             : 0,
+#     'mroll'             : 0,
+#     'droll'             : 0,
+#     'halfmode'          : False,
+#     "entrain"           : True,
+#     "eof_forcing"       : True,
+#     "Td_corr"           : False, # Set to True if lbd_d is provided as a correlation, rather than 1/months
+#     "lbd_e"             : None,
+#     "Tforce"            : None,
+#     }
+
+# expparams_sss   = {
+#     'varname'           : "SSS",
+#     'bbox_sim'          : [-80,0,20,65],
+#     'nyrs'              : 1000,
+#     'runids'            : ["run%02i" % i for i in np.arange(0,10,1)],
+#     'runid_path'        : "SST_SSS_LHFLX",#"SST_EOF_Qek_pilot", # If not None, load a runid from another directory
+#     'Fprime'            : None,
+#     'PRECTOT'           : None, # No Precip
+#     'LHFLX'             : "CESM1_HTR_FULL_Eprime_EOF_nomasklag1_nroll0_NAtl_corrected_EnsAvg.nc",
+#     'h'                 : "CESM1_HTR_FULL_HMXL_NAtl_EnsAvg.nc",
+#     'lbd_d'             : False,
+#     'Sbar'              : "CESM1_HTR_FULL_Sbar_NAtl_EnsAvg.nc",
+#     'beta'              : None, # If None, just compute entrainment damping
+#     'kprev'             : "CESM1_HTR_FULL_kprev_NAtl_EnsAvg.nc",
+#     'lbd_a'             : None, # NEEDS TO BE CONVERTED TO 1/Mon !!!
+#     'Qek'               : None, # Must be in W/m2
+#     'convert_Fprime'    : False,
+#     'convert_lbd_a'     : False,
+#     'convert_PRECTOT'   : True,
+#     'convert_LHFLX'     : True,
+#     'froll'             : 0,
+#     'mroll'             : 0,
+#     'droll'             : 0,
+#     'halfmode'          : False,
+#     "entrain"           : True,
+#     "eof_forcing"       : True,
+#     "Td_corr"           : False, # Set to True if lbd_d is provided as a correlation, rather than 1/months
+#     "lbd_e"             : "CESM1LE_HTR_FULL_lbde_Bcorr3_lbda_qnet_damping_nomasklag1_EnsAvg.nc",
+#     "Tforce"            : "SST_SSS_LHFLX",
+#     }
+
+
 
 # Constants
 dt    = 3600*24*30 # Timestep [s]
@@ -216,12 +281,15 @@ for vv in range(2):
         if type(expparams[pname])==str: # If String, Load from input folder
             
             # Load ds
-            if (expparams['varname'] == "SST") and (pname =="Fprime"):
-                da_varname  = "LHFLX" # Swap to LHFLX for now
+            if (expparams['varname'] == "SST") and (pname =="Fprime") and "Fprime" not in expname:
+                
+                da_varname   = "LHFLX" # Swap to LHFLX for now
                 varname_swap = True # True so "Fprime" is input as da_varname later
                 swapname     = "Fprime" 
+                
             else:
                 varname_swap = False
+                
                 #inputs_type['Fprime'] = 'forcing' # Add extra Fprime variable
             ds = xr.open_dataset(input_path + ptype + "/" + expparams[pname])[da_varname]
             
@@ -385,10 +453,7 @@ for vv in range(2):
     froll  = expparams['froll']
     droll  = expparams['droll']
     mroll  = expparams['mroll']
-
-
     
-    #%%
     for nr in range(nruns):
         
         #% Prepare White Noise timeseries ----------------------------------------
@@ -417,7 +482,7 @@ for vv in range(2):
             wn = np.random.normal(0,1,noise_size) # [Yr x Mon x Mode]
             np.save(noisefile,wn)
         
-        #%% Do Conversions for Model Run ------------------------------------------
+        #% Do Conversions for Model Run ------------------------------------------
         if nr == 0: # Only perform this once
             
             # Apply roll/shift to seasonal cycle
@@ -616,7 +681,7 @@ for vv in range(2):
         
         #  ------------------------------------------------------------------------
         
-        #%% Integrate the model
+        #% Integrate the model
         if expparams['entrain'] is True:
             outdict = scm.integrate_entrain(smconfig['h'],smconfig['kprev'],smconfig['lbd_a'],smconfig['forcing'],
                                             Tdexp=smconfig['lbd_d'],beta=smconfig['beta'],
@@ -626,7 +691,7 @@ for vv in range(2):
             
         
         
-        #%% Save the output
+        #% Save the output
         var_out  = outdict['T']
         timedim  = xr.cftime_range(start="0001",periods=var_out.shape[-1],freq="MS",calendar="noleap")
         cdict    = {
