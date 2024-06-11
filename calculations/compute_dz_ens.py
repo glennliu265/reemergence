86c74bb35ecf71c372c8beea624df800ce1b6c43
Glenn Liu <glenn.y.liu@gmail.com>
2024-06-11 13:45:39 -0400

    Compute 2nd derivative for TEMP/SALT

--- calculations/compute_dz_ens.py
+++ calculations/compute_dz_ens.py
@@ -5,7 +5,7 @@
 Copied upper section of visualize_dz
 
 Compute the vertical gradient at the detrainment depth
-
+Also compute the second derivative (for diffusion)
 
 Created on Tue May 21 10:41:52 2024
 
@@ -65,6 +65,9 @@ import scm
 
 #%% Other INputs
 
+dz2      = True # Set to True to take the 2nd derivative, False for first.
+
+
 vnames   = ["TEMP","SALT"]
 vcolors  = ["hotpink","navy"]
 vunits   = ["$\degree C$","$psu$"]
@@ -92,62 +95,70 @@ e  = 1
 
 
 # Load the netCDF
-#for vv in range(2):
-vname= vnames[vv]
-
-
-detrain_gradients = np.zeros((42,12,12,nlat,nlon)) * np.nan # [Ens,Entrain Mon, Mon, Lat, Lon]
-for e in range(42):
+for vv in range(2):
+    vname= vnames[vv]
     
-    ncname = "%s%s_NATL_ens%02i.nc" % (dpath,vname,e+1)
-    if e == 32:
-        ncname = proc.addstrtoext(ncname,"_repaired",adjust=-1)
-    ds = xr.open_dataset(ncname).load() # ('time', 'z_t', 'nlat', 'nlon')
     
-    # Compute seasonal cycle
-    ds_scycle = ds[vname].groupby('time.month').mean('time') #  ('month', 'z_t', 'nlat', 'nlon')
-    
-    # Compute centered difference
-    #invar  = ds_scycle.values
-    #z_t    = ds_scycle.z_t.values/100 # Convert cm --> m
-    ds_scycle['z_t'] = ds.z_t.values/100
-    dxdz   = ds_scycle.differentiate('z_t')#np.gradient(invar,z_t,axis=1) # Compute Gradient over Z axis 
-    
-    # Looping for each point
-    for o in tqdm.tqdm(range(nlon)):
-        for a in range(nlat):
-            
-            # Retrieve detrainment depth and gradients at the point
-            hdet_pt = ds_hdetrain.h.isel(ens=e,lat=a,lon=o) # mon
-            lonf=hdet_pt.lon.values.item()
-            latf=hdet_pt.lat.values.item()
-            if lonf<0:
-                lonf = lonf+360
-            dxdz_pt = proc.find_tlatlon(dxdz,lonf,latf,verbose=False) # (month: 12, z_t: 44)
-            
-            # For each entraining month
-            for im in range(12):
-                # Get Detraining Depth
-                
-                hdet = hdet_pt[im].values.item()
-                
-                if np.isnan(hdet):
-                    continue
-                        
-                # Select nearest gradient
-                dxdz_mon = dxdz_pt.sel(z_t=hdet,method='nearest').values
-                detrain_gradients[e,im,:,a,o] = dxdz_mon.copy()
+    detrain_gradients = np.zeros((42,12,12,nlat,nlon)) * np.nan # [Ens,Entrain Mon, Mon, Lat, Lon]
+    for e in range(42):
         
-coords=dict(ens=np.arange(1,43,1),
-    entrain_mon=np.arange(1,13,1),
-    mon=np.arange(1,13,1),
-    lat=lat,
-    lon=lon,
-    )
-da_out  = xr.DataArray(detrain_gradients,coords=coords,dims=coords,name='grad')
-edict   = {'grad':{'zlib':True}}
-outname = "%sCESM1_HTR_%s_Detrain_Gradients.nc" % (outpath,vnames[vv])
-da_out.to_netcdf(outname,encoding=edict,)
+        ncname = "%s%s_NATL_ens%02i.nc" % (dpath,vname,e+1)
+        if e == 32:
+            ncname = proc.addstrtoext(ncname,"_repaired",adjust=-1)
+        ds = xr.open_dataset(ncname).load() # ('time', 'z_t', 'nlat', 'nlon')
+        
+        # Compute seasonal cycle
+        ds_scycle = ds[vname].groupby('time.month').mean('time') #  ('month', 'z_t', 'nlat', 'nlon')
+        
+        # Compute centered difference
+        #invar  = ds_scycle.values
+        #z_t    = ds_scycle.z_t.values/100 # Convert cm --> m
+        ds_scycle['z_t'] = ds.z_t.values/100
+        dxdz   = ds_scycle.differentiate('z_t')#np.gradient(invar,z_t,axis=1) # Compute Gradient over Z axis 
+        
+        if dz2: # Take centered difference (again)
+            print("Taking second derivative")
+            dxdz   = dxdz.differentiate('z_t')
+            
+        
+        # Looping for each point
+        for o in tqdm.tqdm(range(nlon)):
+            for a in range(nlat):
+                
+                # Retrieve detrainment depth and gradients at the point
+                hdet_pt = ds_hdetrain.h.isel(ens=e,lat=a,lon=o) # mon
+                lonf=hdet_pt.lon.values.item()
+                latf=hdet_pt.lat.values.item()
+                if lonf<0:
+                    lonf = lonf+360
+                dxdz_pt = proc.find_tlatlon(dxdz,lonf,latf,verbose=False) # (month: 12, z_t: 44)
+                
+                # For each entraining month
+                for im in range(12):
+                    # Get Detraining Depth
+                    
+                    hdet = hdet_pt[im].values.item()
+                    
+                    if np.isnan(hdet):
+                        continue
+                            
+                    # Select nearest gradient
+                    dxdz_mon = dxdz_pt.sel(z_t=hdet,method='nearest').values
+                    detrain_gradients[e,im,:,a,o] = dxdz_mon.copy()
+            
+    coords=dict(ens=np.arange(1,43,1),
+        entrain_mon=np.arange(1,13,1),
+        mon=np.arange(1,13,1),
+        lat=lat,
+        lon=lon,
+        )
+    da_out  = xr.DataArray(detrain_gradients,coords=coords,dims=coords,name='grad')
+    edict   = {'grad':{'zlib':True}}
+    if dz2:
+        outname = "%sCESM1_HTR_%s_Detrain_Gradients_dz2.nc" % (outpath,vnames[vv])
+    else:
+        outname = "%sCESM1_HTR_%s_Detrain_Gradients.nc" % (outpath,vnames[vv])
+    da_out.to_netcdf(outname,encoding=edict,)
 
 #%% New section (compute the local standard deviation, for normalization purposes)
 
@@ -214,6 +225,11 @@ for vv in range(2):
     outname = "%sCESM1_HTR_%s_Detrain_Stdev.nc" % (outpath,vnames[vv])
     da_out.to_netcdf(outname,encoding=edict,)
     print("Saved to %s"% outname)
+
+
+
+
+
 
 # -----------------------------------
 #%% Compute surface variance
