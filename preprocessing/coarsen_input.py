9145598474b08a39db11fca8198017fc5cabd533
Glenn Liu <glenn.y.liu@gmail.com>
2024-07-11 14:21:04 -0400

    Add coarsen vs re-estimate comparison
    Compare how HFF looks like when directly coarsening the parameter or re-estimating after coarsening the raw variables

--- preprocessing/coarsen_input.py
+++ preprocessing/coarsen_input.py
@@ -58,6 +58,8 @@ vnames      = ["SALT","TEMP"]
 #%% Indicate the file name
 
 # Load the Target Dataset
+#target_nc   = "CESM1_HTR_FULL_qnet_damping_nomasklag1_EnsAvg.nc"
+#varname     = "damping"
 target_nc   = "CESM1_HTR_FULL_corr_d_SALT_detrendensmean_lagmax3_interp1_imshift1_dtdepth1_EnsAvg.nc"
 varname     = "lbd_d"
 target_path = dpath
@@ -65,7 +67,6 @@ dstarg      = xr.open_dataset(target_path + target_nc)[varname].load()
 target_var  = dstarg.transpose('mon','lat','lon').data
 latold      = dstarg.lat.data
 lonold      = dstarg.lon.data
-
 
 # Load and get the reference grid
 ref_nc      = "cesm1le_htr_5degbilinear_Fprime_EOF_corrected_cesm1le5degqnet_nroll0_perc090_NAtl_EnsAvg.nc"
@@ -78,7 +79,7 @@ dsref       = xr.open_dataset(ref_path + ref_nc).load()
 lonnew      = dsref.lon.data
 latnew      = dsref.lat.data
 
-outnc       = dpath + "CESM1_HTR_FULL_corr_d_SALT_detrendensmean_lagmax3_interp1_imshift1_dtdepth1_EnsAvg_coarsen5deg.nc"
+outnc       = dpath + "CESM1_HTR_FULL_qnet_damping_nomasklag1_coarsen5deg.nc"
 
 print("Coarsening: %s" % target_nc)
 print("\tTarget Grid from \t: %s" % ref_nc)
@@ -101,11 +102,111 @@ edict   = proc.make_encoding_dict(dstarg)
 daout.to_netcdf(outnc,encoding=edict)
 print("Saved output to %s" % outnc)
 
-#%% For debugging, compare the two
+# --------------------------------- -------------------------------------------
+# ---------------------------------- -----------------------------------------|
+#%% For debugging, compare the two -- ----------------------------------------<
+# ---------------------------------- -----------------------------------------|
+# --------------------------------- -------------------------------------------
+# Currently works for eat flux feedback
+# Also load re-estimated output
+nc_reest = "cesm1_htr_5degbilinear_qnet_damping_damping_cesm1le5degqnetDamp_EnsAvg.nc"
+dsreest  = xr.open_dataset(dpath + nc_reest).damping.load()
 
-im = 11
+#%%
 
 
+for im in range(12):
+    
+    # 
+    dsraw    = dstarg.isel(mon=im)
+    dscoarse = daout.isel(mon=im)
+    dsre     = dsreest.isel(mon=im)
+    
+    #% Plot Everything
+    
+    import matplotlib as mpl
+    import cartopy.crs as ccrs
+    from amv import viz
+    
+    bboxplot                        = [-80,0,10,65]
+    mpl.rcParams['font.family']     = 'Avenir'
+    mons3                           = proc.get_monstr(nletters=3)
+    
+    fsz_tick                        = 18
+    fsz_axis                        = 20
+    fsz_title                       = 16
+    
+    #rhocrit = proc.ttest_rho(0.05,2,86)
+    proj                            = ccrs.PlateCarree()
+    
+    
+    
+    plotvars    = [dsraw,dscoarse,dsre]
+    plotnames   = ["Raw","Coarsened","Re-estimated"]
+    
+    
+    fig,axs,_ = viz.init_orthomap(1,3,bboxplot,figsize=(16,8),constrained_layout=True,centlat=45)
+    
+    for yy in range(3):
+        ax  = axs.flatten()[yy]
+        blb = viz.init_blabels()
+        
+        if yy !=0:
+            blb['left']=False
+        else:
+            blb['left']=True
+        blb['lower']=True
+        
+        ax           = viz.add_coast_grid(ax,bboxplot,fill_color="lightgray",fontsize=20,blabels=blb,
+                                        fix_lon=np.arange(-80,10,10),fix_lat=np.arange(0,70,10),grid_color="k")
+        
+        ax.set_title(plotnames[yy],fontsize=fsz_title)
+        
+        pv = plotvars[yy]
+        
+        pcm = ax.pcolormesh(pv.lon,pv.lat,pv.data,
+                            transform=proj,
+                            cmap='cmo.balance',vmin=-35,vmax=35)
+        
+        
+    cb = viz.hcbar(pcm,ax=axs.flatten(),fraction=0.045)
+    cb.set_label("Qnet Heat Flux Feedback")
+    
+    savename = "%sHFF_Coarsen_effects_mon%02i.png" % (figpath,im+1)
+    plt.savefig(savename,dpi=150,bbox_inches='tight')
+    
+    #% Plot the Differences
+    
+    ## None of this is working
+    #dscoarse.reset_index('lon')
+    #dscoarse = dscoarse.drop_isel({'lon':-1})
+    
+    dsre_sel = dsre.sel(lon=slice(-90,-5))
+    dsco_sel = dscoarse.sel(lon=slice(-90,-5))
+    
+    limask = xr.where(np.isnan(dsco_sel),np.nan,1)
+    diff   = dsre_sel * limask - dsco_sel
+    
+    fig,ax,_ = viz.init_orthomap(1,1,bboxplot,figsize=(16,4.5),constrained_layout=True,centlat=45)
+    
+    
+    ax           = viz.add_coast_grid(ax,bboxplot,fill_color="lightgray",fontsize=20,
+                                    fix_lon=np.arange(-80,10,10),fix_lat=np.arange(0,70,10),grid_color="k")
+    
+    
+    pv = diff
+    pcm = ax.pcolormesh(pv.lon,pv.lat,pv.data,
+                        transform=proj,
+                        cmap='cmo.balance',vmin=-10,vmax=10)
+    ax.set_title("Differences (Restimate - Coarsened)")
+    cb = viz.hcbar(pcm,ax=ax,fraction=0.045)
+    cb.set_label("Qnet Heat Flux Feedback")
+    
+    savename = "%sHFF_Coarsen_effects_diff_mon%02i.png" % (figpath,im+1)
+    plt.savefig(savename,dpi=150,bbox_inches='tight')
+    
+    
+#%%
 
 
 
