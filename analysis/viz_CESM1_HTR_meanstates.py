e44b63863b5cd2bf9436019c8bc6143aec656b30
Glenn Liu <glenn.y.liu@gmail.com>
2024-07-23 15:23:02 -0400

    TCM Updates, add GS, REI (SSS)
    Add GS plot from uvel/vvel
    Add locator plot for thesis committee meeting using SSS REI as the reference
    Add regional bounding boxes

--- analysis/viz_CESM1_HTR_meanstates.py
+++ analysis/viz_CESM1_HTR_meanstates.py
@@ -71,8 +71,8 @@ bboxplot                    = [-80,0,20,65]
 mpl.rcParams['font.family'] = 'Avenir'
 mons3                       = proc.get_monstr(nletters=3)
 fsz_tick                    = 18
-fsz_axis                    = 14
-fsz_title                   = 16
+fsz_axis                    = 22
+fsz_title                   = 28
 
 proj                        = ccrs.PlateCarree()
 
@@ -90,21 +90,37 @@ ds_sst          = dl.load_monmean('SST')
 tlon  = ds_uvel.TLONG.mean('ens').values
 tlat  = ds_uvel.TLAT.mean('ens').values
 
+
+# Load Mixed-Layer Depth
+mldpath = input_path + "mld/"
+mldnc   = "CESM1_HTR_FULL_HMXL_NAtl_EnsAvg.nc"
+ds_mld  = xr.open_dataset(mldpath+mldnc).h.load()
+
+#ds_h          = dl.load_monmean('HMXL')
+
 #%% Load Masks
 
 # Load Land Ice Mask
-icemask  = xr.open_dataset(input_path + "masks/CESM1LE_HTR_limask_pacificmask_enssum_lon-90to20_lat0to90.nc")
+icemask     = xr.open_dataset(input_path + "masks/CESM1LE_HTR_limask_pacificmask_enssum_lon-90to20_lat0to90.nc")
 
 
-mask = icemask.MASK.squeeze()
-mask_plot = xr.where(np.isnan(mask),0,mask)#mask.copy()
+mask        = icemask.MASK.squeeze()
+mask_plot   = xr.where(np.isnan(mask),0,mask)#mask.copy()
 
-mask_apply = icemask.MASK.squeeze().values
+mask_apply  = icemask.MASK.squeeze().values
 #mask_plot[np.isnan(mask)] = 0
 
+# Load Gulf Stream
+ds_gs   = dl.load_gs()
+ds_gs   = ds_gs.sel(lon=slice(-90,-50))
+ds_gs2  = dl.load_gs(load_u2=True)
 
-ds_gs = dl.load_gs()
-ds_gs = ds_gs.sel(lon=slice(-90,-50))
+#%% Load REI From a specific run
+
+rei_nc   = "REI_Pointwise.nc"
+rei_path = output_path + "SSS_CESM/Metrics/"
+ds_rei   = xr.open_dataset(rei_path + rei_nc).load().rei
+reiplot  = ds_rei.isel(mon=[1,2],yr=0).mean('mon').mean('ens')
 
 #%% Start Visualization (Ens Mean)
 
@@ -115,6 +131,7 @@ cints_bsf   = np.arange(-50,55,5)
 cints_sst   = np.arange(250,310,2)
 cints_sss   = np.arange(33,39,.3)
 cints_ssh   = np.arange(-200,200,10)
+cints_rei   = np.arange(0,0.65,0.05)
 
 for im in range(12):
     
@@ -176,13 +193,10 @@ rsty            = regiondicts['rsty']
 regplot = [0,1,3]
 nregs   = len(regplot)
 
-
-
 # Initialize Plot and Map
 fig,ax,_    = viz.init_orthomap(1,1,bboxplot,figsize=(18,6.5))
 ax          = viz.add_coast_grid(ax,bbox=bboxplot,fill_color="lightgray")
 ax.set_title("CESM1 Historical Ens. Avg., Ann. Mean",fontsize=fsz_title)
-
 
 # Plot Currents
 plotu = ds_uvel.UVEL.mean('ens').mean('month').values
@@ -197,7 +211,6 @@ pcm     = ax.pcolormesh(plotvar.lon,plotvar.lat,plotvar,transform=proj,zorder=-1
 cb = viz.hcbar(pcm,ax=ax,fraction=0.045)
 cb.set_label("SST ($\degree C$)",fontsize=fsz_axis)
 
-
 # Plot Mean SSS (Contours)
 plotvar = ds_sss.SSS.mean('ens').isel(mon=im).transpose('lat','lon') * mask_apply
 cl = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,
@@ -206,16 +219,124 @@ ax.clabel(cl)
 
 for ir in range(nregs):
     rr = regplot[ir]
+    ls_in = rsty[rr]
+    if ir == 2:
+        ls_in = 'dashed'
+        
     rbbx = bboxes[rr]
-    viz.plot_box(rbbx,color=rcols[rr],linestyle=rsty[rr],leglab=regions_long[rr],linewidth=2.5,return_line=True)
+    viz.plot_box(rbbx,color=rcols[rr],linestyle=ls_in,leglab=regions_long[rr],linewidth=2.5,return_line=True)
 
 
 # Plot Gulf Stream Position
-ax.plot(ds_gs.lon,ds_gs.lat.mean('ens'),transform=proj,lw=1.75,c="k")
+#ax.plot(ds_gs.lon,ds_gs.lat.mean('ens'),transform=proj,lw=1.75,c="k",ls='dashed')
+ax.plot(ds_gs2.lon.mean('mon'),ds_gs2.lat.mean('mon'),transform=proj,lw=1.75,c='k',ls='dashdot')
+
+# Plot Ice Edge
+ax.contour(icemask.lon,icemask.lat,mask_plot,colors="cyan",linewidths=2.5,
+           transform=proj,levels=[0,1],zorder=-1)
+
 
 
 figname = "%sCESM1_Locator_MeanState.png" % (figpath,)
 plt.savefig(figname,dpi=150,bbox_inches='tight')
+
+#%% Same Plot as above, but focus on SSS re-emergence (locator for TCM presentation)
+
+fsz_tick = 14
+
+# Initialize Plot and Map
+fig,ax,_    = viz.init_orthomap(1,1,bboxplot,figsize=(18,6))
+ax          = viz.add_coast_grid(ax,bbox=bboxplot,fill_color="lightgray",fontsize=fsz_tick)
+#ax.set_title("CESM1 Historical Ens. Avg., Ann. Mean",fontsize=fsz_title)
+
+
+# Plot Currents
+qint  = 1
+plotu = ds_uvel.UVEL.mean('ens').mean('month').values
+plotv = ds_vvel.VVEL.mean('ens').mean('month').values
+ax.quiver(tlon[::qint,::qint],tlat[::qint,::qint],plotu[::qint,::qint],plotv[::qint,::qint],
+          color='mediumblue',transform=proj,alpha=0.35,)#scale=1e3)
+
+# Plot REI Index
+plotvar = reiplot
+pcm     = ax.contourf(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+                      cmap='cmo.deep',levels=cints_rei,zorder=-3)
+cl     = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,linewidths=0.75,
+                      colors='w',levels=cints_rei,zorder=-2)
+ax.clabel(cl)
+
+
+for ir in range(nregs):
+    rr = regplot[ir]
+    rbbx = bboxes[rr]
+    
+    ls_in = rsty[rr]
+    if ir == 2:
+        ls_in = 'dashed'
+    
+    viz.plot_box(rbbx,color=rcols[rr],linestyle=ls_in,leglab=regions_long[rr],linewidth=2.5,return_line=True)
+
+
+# Plot Gulf Stream Position
+ax.plot(ds_gs2.lon.mean('mon'),ds_gs2.lat.mean('mon'),transform=proj,lw=2.5,c='k',ls='dashdot')
+
+# Plot Ice Edge
+ax.contour(icemask.lon,icemask.lat,mask_plot,colors="cyan",linewidths=2.5,
+           transform=proj,levels=[0,1],zorder=-1)
+
+
+cb = viz.hcbar(pcm,ax=ax,fraction=0.036)
+cb.ax.tick_params(labelsize=fsz_tick)
+cb.set_label("Re-emergence Index",fontsize=fsz_axis)
+
+figname = "%sCESM1_Locator_MeanState_REM.png" % (figpath,)
+plt.savefig(figname,dpi=150,bbox_inches='tight')
+
+#%% Mini Locator
+
+fsz_tick  = 14
+bblocator = [-80,-20,30,65] 
+# Initialize Plot and Map
+fig,ax,_    = viz.init_orthomap(1,1,bblocator,figsize=(4,2),centlon=-50)
+ax          = viz.add_coast_grid(ax,bbox=bblocator,fill_color="lightgray",fontsize=0)
+#ax.set_title("CESM1 Historical Ens. Avg., Ann. Mean",fontsize=fsz_title)
+
+# # Plot Currents
+qint  = 2
+plotu = ds_uvel.UVEL.mean('ens').mean('month').values
+plotv = ds_vvel.VVEL.mean('ens').mean('month').values
+ax.quiver(tlon[::qint,::qint],tlat[::qint,::qint],plotu[::qint,::qint],plotv[::qint,::qint],
+          color='mediumblue',transform=proj,alpha=0.35,)#scale=1e3)
+
+# Plot REI Index
+plotvar = reiplot
+pcm     = ax.contourf(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+                      cmap='cmo.deep',levels=cints_rei,zorder=-3)
+
+for ir in range(nregs):
+    rr = regplot[ir]
+    rbbx = bboxes[rr]
+    
+    ls_in = rsty[rr]
+    if ir == 2:
+        ls_in = 'dashed'
+    
+    viz.plot_box(rbbx,color=rcols[rr],linestyle=ls_in,leglab=regions_long[rr],linewidth=1.5,return_line=True)
+
+# # Plot Gulf Stream Position
+ax.plot(ds_gs2.lon.mean('mon'),ds_gs2.lat.mean('mon'),transform=proj,lw=1.5,c='k',ls='dashdot')
+
+# Plot Ice Edge
+ax.contour(icemask.lon,icemask.lat,mask_plot,colors="cyan",linewidths=1.5,
+           transform=proj,levels=[0,1],zorder=-1)
+
+# cb = viz.hcbar(pcm,ax=ax,fraction=0.036)
+# cb.ax.tick_params(labelsize=fsz_tick)
+# cb.set_label("Re-emergence Index",fontsize=fsz_axis)
+
+figname = "%sCESM1_Locator_MeanState_REM_Locator.png" % (figpath,)
+plt.savefig(figname,dpi=150,bbox_inches='tight')
+
 # -----------------------------------------------------------------------------
 #%% Load and plot currents relative to Stochastic Model Fit (Diff by Lag, as computed
 # in visualize_rei_acf.py)
@@ -302,7 +423,47 @@ plotv = ds_vvel.VVEL.mean('ens').mean('month').values
 ax.quiver(tlon[::qint,::qint],tlat[::qint,::qint],plotu[::qint,::qint],plotv[::qint,::qint],
           color='navy',transform=proj,alpha=0.75)
 
+#%% Visualize Month of the deepest MLD
 
+#cmap = mpl.colors.ListedColormap([''])
+
+plotvar   = ds_mld.argmax('mon') + 1
+cints     = np.arange(0,13,1)
+
+cmap = plt.colormaps['twilight']
+norm = mpl.colors.BoundaryNorm(cints, ncolors=12, clip=True)
+
+fig,ax,_  = viz.init_orthomap(1,1,bboxplot,figsize=(16,12),centlon=-40)
+ax        = viz.add_coast_grid(ax,bbox=bboxplot,fill_color='lightgray')
+
+
+# pcm = ax.pcolormesh(plotvar.lon,plotvar.lat,
+#                     plotvar*mask,transform=proj,
+#                     cmap=cmap,vmin=1,vmax=12)
+
+
+pcm = ax.contourf(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+            cmap=cmap,levels=cints,zorder=-4)
+cb  = viz.hcbar(pcm)
+
+# cl = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+#             levels=cints,zorder=-3,colors='lightgray',linewidth=0.75)
+# ax.clabel(cl,fontsize=fsz_tick)
+
+# Plot Feb
+# pcm = ax.contourf(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+#              colors='darkviolet',levels=[2,3],zorder=-4)
+# cl = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+#             levels=[1,2,3,4],zorder=2,colors='lightgray',linewidth=0.75)
+# ax.clabel(cl,fontsize=fsz_tick)
+
+# Plot Jan
+im = 1
+# pcm = ax.contourf(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+#              cmap='jet',levels=[im,im+1,im+2],zorder=-4)
+# cl = ax.contour(plotvar.lon,plotvar.lat,plotvar,transform=proj,
+#             levels=[im,im+1,i+2],zorder=2,colors='lightgray',linewidth=0.75)
+# ax.clabel(cl,fontsize=fsz_tick)
 
 #%%
 
