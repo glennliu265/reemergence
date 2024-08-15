056e66997cad7dc43ab8ffb8dca31ca257dc7425
Glenn Liu <glenn.y.liu@gmail.com>
2024-08-15 14:29:09 -0400

    Add option to regrid in original Tgrid

--- detrainment_damping/regrid_POP_ML.py
+++ detrainment_damping/regrid_POP_ML.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
@@ -9,8 +6,10 @@ Regrid POP data, but average across the mixed-layer
 Copied  code as [regrid_POP_1level]
 Currently works with UVEL/VVEL Output
 
-Created on Aug 7 13:31:33 2024
+Optionally, remain in pop grid and work with HMXL processed in
+[process_bylevel_ens]
 
+Created on Aug 7 13:31:33 2024
 @author: gliu
 """
 
@@ -57,16 +56,27 @@ loopens     = np.arange(1,43,1)#[32,]
 
 idz         = 0
 
+# Additional Option
+keep_POPgrid = True # Set to True to keep the POP grid
+tol          = 1e-7 # Tolerance for Lat.Lon Differences
+
+
 #%% Indicate which files to process
 
-fns_all = []
+fns_all     = []
+fns_hmxl    = []
 for v in range(len(vnames)):
     vname = vnames[v]
     fns = []
     for e in loopens:
+        # Get filename for variable
         fn = "%socn_var_3d/%s/%s_NATL_ens%02i.nc" % (rawpath,vname,vname,e)
-        
         fns.append(fn)
+        # Get filename for hxml
+        if keep_POPgrid:
+            fnh = "%socn_var_3d/%s_NATL_ens%02i.nc" % (rawpath,"HMXL",e)
+            fns_hmxl.append(fnh)
+        
     fns_all.append(fns)
 
 # bounding box for final output
@@ -80,91 +90,232 @@ dstl    = xr.open_dataset(outpath+fnlat)
 tlat    = dstl.TLAT.values
 tlon    = dstl.TLONG.values
 
+
+
 #%% Retrieve dimensions of CESM1 from another file
 
-mldnc   = "CESM1_HTR_FULL_HMXL_NAtl.nc"
-dsh     = xr.open_dataset(mldpath+mldnc).h
-
-dshreg  = dsh.sel(lon=slice(bbox[0],bbox[1]),lat=slice(bbox[2],bbox[3]))
-
-outlat  = dshreg.lat.values
-outlon  = dshreg.lon.values
-
-#%% Seems the easier way might just be to do this loopwise (silly but I guess...)
-
-nvars   = len(vnames)
-
-for v in range(nvars):   
-    vname = vnames[v] 
-    for e in tqdm(range(nens)):
+if keep_POPgrid:
+    # First, compute the mean seasonal MLD
+    nlat,nlon = tlon.shape
+    
+    # Get TLAT and TLONG
+    dsh     = xr.open_dataset(fns_hmxl[0]).load()
+    tlat = dsh.TLAT.data
+    tlon = dsh.TLONG.data
+    
+    tdim     = dict(nlat=dsh.nlat,nlon=dsh.nlon)
+    tlong_ds = xr.DataArray(dsh.TLONG.data,coords=tdim,name='TLONG')
+    tlat_ds  = xr.DataArray(dsh.TLAT.data,coords=tdim,name='TLAT')
+    
+    
+    # Directly Go into Loop and process pointwise
+    nvars   = len(vnames)
+    for e in tqdm(range(nens)): # Loop by ensemble first
         
-        st = time.time()
-        fn = fns_all[v][e]
-        # Open the Dataset
-        ds          = xr.open_dataset(fn).isel(z_t=idz).load()#.load()
-        dsl         = ds.assign(lon=(['nlat','nlon'],tlon),lat=(['nlat','nlon'],tlat))
-        nlon,nlat   = len(outlon),len(outlat)
+        # Load Mixed Layer Depth, Compute mean seasonal Cycle
+        dsh     = xr.open_dataset(fns_hmxl[e]).load()
+        hcycle  = dsh.HMXL.groupby('time.month').mean('time')
         
-        # Get Dimensions and preallocate
-        ntime,ntlat,ntlon = ds[vname].shape
-        ocn_var_avg = np.zeros((ntime,nlat,nlon))
         
-        # Looping for each point
-        for a in range(nlat):
+        for v in range(nvars):
+            vname = vnames[v] 
             
-            latf   = outlat[a]
+            # Load the Variable
+            st = time.time()
+            fn = fns_all[v][e]
             
-            for o in tqdm(range(nlon)): # Took (1h 11 min if you don't load, 2 sec if you load, T-T)
+            # Open the Dataset
+            st1         = time.time()
+            ds          = xr.open_dataset(fn).load()
+            #dsl         = ds.assign(lon=(['nlat','nlon'],tlon),lat=(['nlat','nlon'],tlat))
+            #nlon,nlat   = len(outlon),len(outlat)
+            print("Loaded Dataset in %.2fs" % (time.time()-st1))
+            
+            # Check Region Extent and TLONG/TLAT
+            if not (ds.TLONG.shape == dsh.TLONG.shape):
+                print("Warning, shapes are not equal. Please make sure they are cropped to the same region...")
+                break
+            if not np.all(np.abs(ds.TLONG.data - dsh.TLONG.data) < tol):
+                print("Warning, not all longitudes are equal/within tolerance %.2e" % tol)
+                break
+            if not np.all(np.abs(ds.TLAT.data - dsh.TLAT.data) < tol):
+                print("Warning, not all latitudes are equal/within tolerance %.2e" % tol)
+                break
+            
+            ntime,nz,nlat,nlon   = ds[vname].shape
+            mld_avg_var          = np.zeros((ntime,nlat,nlon)) * np.nan
+            
+            for o in tqdm(range(nlon)):
                 
-                # Longitudes
-                lonf = outlon[o]
-                if lonf < 0:
-                    lonf += 360
-                
-                # Get the nearest point
-                outids = proc.get_pt_nearest(dsl,lonf,latf,tlon_name="lon",tlat_name="lat",returnid=True,debug=False)
-                dspt   = dsl.isel(nlat=outids[0],nlon=outids[1])
-                
-                ocn_var_avg[:,a,o] = dspt[vname].data
-                # End Longitude Loop ---
-                
-            # End Latitude Loop ---
-                
-        #% Apply mask based on h
-        mask                    = np.sum(dshreg.values,(0,1))
-        mask[~np.isnan(mask)]   = 1
-        da_mask                 = xr.DataArray(mask,coords=dict(lat=outlat,lon=outlon))
-        
-        # Save Regridded Data
-        coords                  = dict(time=ds.time,lat=dshreg.lat,lon=dshreg.lon,)
-        da_out                  = xr.DataArray(ocn_var_avg * mask[None,:,:],coords=coords,dims=coords,name=vname)
-        
-        
-        savename = proc.addstrtoext(fn,"_regridNN",adjust=-1)
-        edict    = proc.make_encoding_dict(da_out)
-        da_out.to_netcdf(savename,encoding=edict)
-        print("Saved output in %.2fs" % (time.time()-st))
+                for a in range(nlat):
+                    
+                    # Get Lat and Lon
+                    lonf = tlon[a,o]
+                    latf = tlat[a,o]
+                    
+                    # Locate the depth
+                    hpt  = hcycle.isel(nlon=o,nlat=a)#(mon=im,ens=e,lon=o,lat=a).item()
+                    if np.any(np.isnan(hpt)).item():
+                        continue
+                    
+                    # Get the point
+                    dspt = ds[vname].isel(nlon=o,nlat=a)
+                    
+                    ptval_bymon = []
+                    for im in range(12):
+                        # Select points for that month
+                        dspt_mon = dspt.sel(time=ds.time.dt.month.isin([im+1]))
+                        
+                        # Restrict to depth
+                        dspt_mon = dspt_mon.sel(z_t=slice(0,hpt.isel(month=im).data.item()))
+                        
+                        # Take mean over mixed-layer depth
+                        if len(dspt_mon.z_t) < 1: # Exit if no MLD was found...
+                            print("Warning, values no found within the mixed-layer depth range at Lon=%i Lat=%i (month %i..." % (o,a,im+1))
+                            continue
+                        else:
+                            dspt_mon = dspt_mon.mean('z_t')
+                            ptval_bymon.append(dspt_mon)
+                    
+                    if len(ptval_bymon) < 12: # Exit if not all months had a MLD value...
+                        print("Warning, less than 12 months were found at Lon=%i Lat=%i..." % (o,a))
+                    else:
+                        ptval_bymon = xr.concat(ptval_bymon,dim='time')
+                        
+                        mld_avg_var[:,a,o] = ptval_bymon.data
+            
+            coords   = dict(time=ds.time,nlat=ds.nlat,nlon=ds.nlon)
+            ds_out   = xr.DataArray(mld_avg_var,coords=coords,dims=coords,name=vname)
+            ds_out   = xr.merge([ds_out,tlong_ds,tlat_ds])
+            edict    = proc.make_encoding_dict(ds_out)
+            
+            savename = proc.addstrtoext(fn,"_MLDAVG_POP",adjust=-1)
+            ds_out.to_netcdf(savename,encoding=edict)
+            print("Saved %s" % savename)
+            
+            # End Variable Loop
         # End Ensemble Loop
-    # End Variable Loop
+    
+    # Reopen and Merge all the files
+    
+    for v in range(len(vnames)):
+        
+        # Variables 
+        vname = vnames[v]
+        fns   = []
+        for e in loopens:
+            fn = "%socn_var_3d/%s/%s_NATL_ens%02i_MLDAVG_POP.nc" % (rawpath,vname,vname,e)
+            fns.append(fn)
+        
+        # Indicate which datasets
+        ds_all  = xr.open_mfdataset(fns,concat_dim='ens',combine='nested').load()
+        
+        # Save the ouput as a bulk file
+        edict   = proc.make_encoding_dict(ds_all)
+        fn_out  = "%socn_var_3d/%s/%s_NATL_AllEns_MLDAVG_POP.nc" % (rawpath,vname,vname)
+        ds_all.to_netcdf(fn_out,encoding=edict)
+                
+    
+    
+else:
+    # Otherwise... Load HMXL and do stuff
+    mldnc   = "CESM1_HTR_FULL_HMXL_NAtl.nc"
+    dsh     = xr.open_dataset(mldpath+mldnc).h
+    
+    dshreg  = dsh.sel(lon=slice(bbox[0],bbox[1]),lat=slice(bbox[2],bbox[3]))
+    
+    outlat  = dshreg.lat.values
+    outlon  = dshreg.lon.values
+
+        
+    #%% Seems the easier way might just be to do this loopwise (silly but I guess...)
+    
+    nvars   = len(vnames)
+    
+    for v in range(nvars):   
+        vname = vnames[v] 
+        for e in tqdm(range(nens)):
+            
+            st = time.time()
+            fn = fns_all[v][e]
+            # Open the Dataset
+            st1 = time.time()
+            ds          = xr.open_dataset(fn).load()
+            dsl         = ds.assign(lon=(['nlat','nlon'],tlon),lat=(['nlat','nlon'],tlat))
+            nlon,nlat   = len(outlon),len(outlat)
+            print("Loaded Dataset in %.2fs" % (time.time()-st1))
+            
+            # Get Dimensions and preallocate
+            ntime,nz,ntlat,ntlon = ds[vname].shape
+            ocn_var_avg = np.zeros((ntime,nlat,nlon))
+            
+            # Looping for each point
+            for a in range(nlat):
+                
+                latf   = outlat[a]
+                
+                for o in tqdm(range(nlon)): # Took (1h 11 min if you don't load, 2 sec if you load, T-T)
+                    
+                    
+                    # Longitudes
+                    lonf = outlon[o]
+                    if lonf < 0:
+                        lonf += 360
+                        
+                    for t in range(ntime):
+                        # Get the month
+                        im   = ds.time.isel(time=t).time.item().month - 1 # Month Index
+                        
+                        # Locate the depth (and convert to cm)
+                        hpt  = dshreg.isel(mon=im,ens=e,lon=o,lat=a).item() * 100
+                        
+                        # Get the nearest point
+                        outids = proc.get_pt_nearest(dsl,lonf,latf,tlon_name="lon",tlat_name="lat",returnid=True,debug=False)
+                        dspt   = dsl.isel(nlat=outids[0],nlon=outids[1],time=t)
+                        
+                        # Slice to depth and average
+                        dspt_mldavg                = dspt.sel(z_t = slice(0,hpt)).mean('z_t')
+                        ocn_var_avg[t,a,o]         = dspt_mldavg[vname].data
+                        
+                    # End Longitude Loop ---
+                    
+                # End Latitude Loop ---
+                    
+            #% Apply mask based on h
+            mask                    = np.sum(dshreg.values,(0,1))
+            mask[~np.isnan(mask)]   = 1
+            da_mask                 = xr.DataArray(mask,coords=dict(lat=outlat,lon=outlon))
+            
+            # Save Regridded Data
+            coords                  = dict(time=ds.time,lat=dshreg.lat,lon=dshreg.lon,)
+            da_out                  = xr.DataArray(ocn_var_avg * mask[None,:,:],coords=coords,dims=coords,name=vname)
+            
+            
+            savename = proc.addstrtoext(fn,"_regridNN_MLavg",adjust=-1)
+            edict    = proc.make_encoding_dict(da_out)
+            da_out.to_netcdf(savename,encoding=edict)
+            print("Saved output in %.2fs" % (time.time()-st))
+            # End Ensemble Loop
+        # End Variable Loop
     
 #%% Open all files and save
 
-for v in range(len(vnames)):
+# for v in range(len(vnames)):
     
-    # Variables 
-    vname = vnames[v]
-    fns   = []
-    for e in loopens:
-        fn = "%socn_var_3d/%s/%s_NATL_ens%02i_regridNN.nc" % (rawpath,vname,vname,e)
-        fns.append(fn)
+#     # Variables 
+#     vname = vnames[v]
+#     fns   = []
+#     for e in loopens:
+#         fn = "%socn_var_3d/%s/%s_NATL_ens%02i_regridNN_MLavg.nc" % (rawpath,vname,vname,e)
+#         fns.append(fn)
     
-    # Indicate which datasets
-    ds_all  = xr.open_mfdataset(fns,concat_dim='ens',combine='nested').load()
+#     # Indicate which datasets
+#     ds_all  = xr.open_mfdataset(fns,concat_dim='ens',combine='nested').load()
     
-    # Save the ouput as a bulk file
-    edict   = proc.make_encoding_dict(ds_all)
-    fn_out  = "%socn_var_3d/%s/%s_NATL_AllEns_regridNN.nc" % (rawpath,vname,vname)
-    ds_all.to_netcdf(fn_out,encoding=edict)
+#     # Save the ouput as a bulk file
+#     edict   = proc.make_encoding_dict(ds_all)
+#     fn_out  = "%socn_var_3d/%s/%s_NATL_AllEns_regridNN_MLavg.nc" % (rawpath,vname,vname)
+#     ds_all.to_netcdf(fn_out,encoding=edict)
 
 
 
