661d959a69be3227352ce0bd210ef698f9190d84
Glenn Liu <glenn.y.liu@gmail.com>
2024-09-12 15:28:56 -0400

    Updated Eprime/LHFLX/Precip Forcing correction
    - Now runs again for CESM1 HTR (original resolution)
    - Made sure to anomalize and detrend the data
    - Added option to take the ensemble average first
    - Note that the crop_sm step does not work currently...

--- preprocessing/correct_eof_forcing_SSS.py
+++ preprocessing/correct_eof_forcing_SSS.py
@@ -90,6 +90,7 @@ procpath    = pathdict['procpath']
 #figpath   = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/02_Figures/20240411/"
 datpath   = pathdict['raw_path']
 outpath   = pathdict['input_path']+"forcing/"
+rawpath   = pathdict['raw_path']
 
 
 #%% User Edits
@@ -99,7 +100,7 @@ outpath   = pathdict['input_path']+"forcing/"
 eof_thres = 0.90
 bbox_crop = [-90,0,0,90]
 # Indicate dataset name
-dataset = "cesm1le_htr_5degbilinear"
+dataset = "CESM1_HTR"
 
 
 if dataset == "CESM1_HTR":
@@ -110,19 +111,19 @@ if dataset == "CESM1_HTR":
     rollstr   = "nroll0"
 
     # Load EOF results
-    nceof     = datpath + "EOF_Monthly_NAO_EAP_Fprime_%s_%s_NAtl.nc" % (dampstr,rollstr)
+    nceof     = rawpath + "EOF_Monthly_NAO_EAP_Fprime_%s_%s_NAtl.nc" % (dampstr,rollstr)
     
     # Load Fprime
     ncfprime  = "CESM1_HTR_FULL_Fprime_timeseries_%s_%s_NAtl.nc" % (dampstr,rollstr)
     
     # Load Evap, Precip
-    ncevap    = "CESM1_HTR_FULL_Eprime_nroll0_NAtl.nc"
-    ncprec    = "CESM1_HTR_FULL_PRECTOT_NAtl.nc"
+    ncevap    = rawpath + "CESM1_HTR_FULL_Eprime_timeseries_LHFLXnomasklag1_nroll0_NAtl.nc"#"CESM1_HTR_FULL_Eprime_nroll0_NAtl.nc"
+    ncprec    = rawpath + "PRECTOT_HTR_FULL.nc"#"CESM1_HTR_FULL_PRECTOT_NAtl.nc"
 
     # Load EOF Regression output
-    ncprec_eof = "CESM1_HTR_FULL_PRECTOT_EOF_nomasklag1_nroll0_NAtl.nc"
+    ncprec_eof = outpath + "CESM1_HTR_FULL_PRECTOT_EOF_nomasklag1_nroll0_NAtl.nc"
     #ncevap_eof = "CESM1_HTR_FULL_LHFLX_EOF_nomasklag1_nroll0_NAtl.nc"
-    ncevap_eof = "CESM1_HTR_FULL_Eprime_EOF_nomasklag1_nroll0_NAtl.nc"
+    ncevap_eof = outpath + "CESM1_HTR_FULL_Eprime_EOF_nomasklag1_nroll0_NAtl.nc"
     
 elif dataset == "cesm1le_htr_5degbilinear":
     
@@ -172,6 +173,12 @@ bbox_crop   = [-90,0,0,90]
 bbox        = [-80,0,0,65]
 debug       = True
 
+
+def anomalize(ds):
+    ds = ds - ds.mean('ens')
+    ds = proc.xrdeseason(ds)
+    return ds
+
 #%% Procedure
 
 # (1) Load EOF Results, compute variance explained
@@ -185,16 +192,25 @@ varexp   = dseof.varexp.mean('ens') # (mode: 86, mon: 12)
 
 # (2) Load  Evap, Precip to compute Monthly Stdev 
 dsevap    = xr.open_dataset(ncevap).load() # (month, ens, lat, lon)
-monvarE   = dsevap.Eprime.groupby('time.month').std('time')
+dsevap    = proc.fix_febstart(dsevap)
+dsevap    = anomalize(dsevap)
+monvarE   = dsevap["LHFLX"].groupby('time.month').std('time')
 monvarE   = monvarE.rename({'month':'mon'})
+
 #dsevap    = dsevap.rename({'month':'mon'})
 dsprec    = xr.open_dataset(ncprec).load() 
+dsprec    = proc.fix_febstart(dsprec)
+dsprec    = anomalize(dsprec)
 monvarP   = dsprec.PRECTOT.groupby('time.month').std('time')
 monvarP   = monvarP.rename({'month':'mon'})
 
 # (3) Load EOF regressions of Evap, Precip
-dsevap_eof = xr.open_dataset(ncevap_eof).load().Eprime # (mode, ens, mon, lat, lon)
+dsevap_eof = xr.open_dataset(ncevap_eof).load().LHFLX # (mode, ens, mon, lat, lon)
 dsprec_eof = xr.open_dataset(ncprec_eof).load().PRECTOT # (mode, ens, mon, lat, lon)
+
+# Make dsure variables are the same
+dsevap_eof,monvarE = proc.resize_ds([dsevap_eof,monvarE])
+dsprec_eof,monvarP = proc.resize_ds([dsprec_eof,monvarP])
 
 #%% 3. Perform EOF filtering (retain enough modes to explain [eof_thres]% of variance for each month)
 
@@ -207,64 +223,116 @@ ds_std     = [monvarE,monvarP]         # Monthly standard deviation (ens, mon , 
 ncnames    = [ncevap_eof,ncprec_eof]
 nvars      = len(vnames)
 
+ensavg_first = True
+
+
+
+
 # By Default, use the longitude coordinates of the regressed variables
 # CAUTION: This script DOES NOT check if longitude is same across all variables
 # Need to write a check for this..
-lon_out    = dsevap_eof.lon
-lat_out    = dsevap_eof.lat
+lon_out    = dsprec_eof.lon
+lat_out    = dsprec_eof.lat
 
 for v in range(nvars): # Loop by Variable
     
-    # Index variables
-    eofvar_in = ds_eof_raw[v].transpose('mode','ens','mon','lat','lon').values
-    monvarfp  = ds_std[v].transpose('ens','mon','lat','lon').values
-    
-    # Perform Filtering
-    eofs_filtered,varexp_cumu,nmodes_needed,varexps_filt=proc.eof_filter(eofvar_in,varexp_in,
-                                                       eof_thres,axis=0,return_all=True)
-    
-    # Compute Stdev of EOFs
-    eofs_std = np.sqrt(np.sum(eofs_filtered**2,0)) # [Ens x Mon x Lat x Lon]
-    
-    # Compute pointwise correction
-    correction_diff = monvarfp - eofs_std
-    
-    # Prepare for output -----
-    corcoords     = dict(ens=ds_std[0].ens,mon=np.arange(1,13,1),lat=lat_out,lon=lon_out)
-    eofcoords     = dict(mode=dseof.mode,ens=ds_std[0].ens,mon=np.arange(1,13,1),lat=lat_out,lon=lon_out)
-    
-    da_correction = xr.DataArray(correction_diff,coords=corcoords,dims=corcoords,name="correction_factor")
-    da_eofs_filt  = xr.DataArray(eofs_filtered,coords=eofcoords,dims=eofcoords  ,name=vnames[v])
-    
-    ds_out        = xr.merge([da_correction,da_eofs_filt])
-    edict         = proc.make_encoding_dict(ds_out)
-    
-    # Save for all ensemble members
-    savename       = proc.addstrtoext(ncnames[v],"_corrected",adjust=-1)
-    ds_out.to_netcdf(savename,encoding=edict)
-    
-    savename_emean = proc.addstrtoext(savename,"_EnsAvg",adjust=-1)
-    ds_out_ensavg  = ds_out.mean('ens')
-    ds_out_ensavg.to_netcdf(savename_emean,encoding=edict)
-    
-    if crop_sm:
+
+    if ensavg_first:
+        # Index variables (take ensemble average)
+        eofvar_in   = np.nanmean(ds_eof_raw[v].transpose('mode','ens','mon','lat','lon').values,1)
+        monvarfp    = np.nanmean(ds_std[v].transpose('ens','mon','lat','lon').data,0)
+        varexp_eavg = varexp_in#np.nanmean(varexp_in,1) # 
+        
+        # Perform Filtering
+        eofs_filtered,varexp_cumu,nmodes_needed,varexps_filt=proc.eof_filter(eofvar_in,varexp_eavg,
+                                                            eof_thres,axis=0,return_all=True)
         
         
+        # Compute Stdev of EOFs
+        eofs_std = np.sqrt(np.sum(eofs_filtered**2,0)) # [Mon x Lat x Lon]
         
-        print("Cropping to region %s" % (regstr_crop))
-        ds_out = proc.lon360to180_xr(ds_out)
+        lon_out    = ds_eof_raw[v].lon
+        lat_out    = ds_eof_raw[v].lat
         
-        ds_out_reg = proc.sel_region_xr(ds_out,bbox_crop)
-        savename_reg = proc.addstrtoext(ncnames[v],"_corrected",adjust=-1).replace(regstr,regstr_crop)
-        ds_out_reg.to_netcdf(savename_reg,encoding=edict)
+        # Compute pointwise correction
+        correction_diff = monvarfp - eofs_std
         
-        savename_emean = proc.addstrtoext(savename_reg,"_EnsAvg",adjust=-1)
-        ds_out_reg_ensavg  = ds_out_reg.mean('ens')
-        ds_out_reg_ensavg.to_netcdf(savename_emean,encoding=edict)
-        print("Saved Ens Avg. Cropped Output to %s" % savename_emean)
+        # Prepare to Save -------------------------
         
-  
+        corcoords     = dict(mon=np.arange(1,13,1),lat=lat_out,lon=lon_out)
+        eofcoords     = dict(mode=ds_eof_raw[0].mode,mon=np.arange(1,13,1),lat=lat_out,lon=lon_out)
         
+        da_correction = xr.DataArray(correction_diff,coords=corcoords,dims=corcoords,name="correction_factor")
+        da_eofs_filt  = xr.DataArray(eofs_filtered,coords=eofcoords,dims=eofcoords  ,name=vnames[v])
+    
+        ds_out        = xr.merge([da_correction,da_eofs_filt])
+        edict         = proc.make_encoding_dict(ds_out)
+        
+        # Save for all ensemble members
+        savename       = proc.addstrtoext(ncnames[v],"_corrected",adjust=-1)
+        savename       = proc.addstrtoext(savename,"_EnsAvgFirst",adjust=-1)
+        ds_out.to_netcdf(savename,encoding=edict)
+        print("Saved output to %s" % savename)
+        
+    else:
+            
+            
+                
+        # Index variables
+        eofvar_in = ds_eof_raw[v].transpose('mode','ens','mon','lat','lon').values
+        monvarfp  = ds_std[v].transpose('ens','mon','lat','lon').values
+            
+        # Perform Filtering
+        eofs_filtered,varexp_cumu,nmodes_needed,varexps_filt=proc.eof_filter(eofvar_in,varexp_in,
+                                                           eof_thres,axis=0,return_all=True)
+        
+        # Compute Stdev of EOFs
+        eofs_std = np.sqrt(np.sum(eofs_filtered**2,0)) # [Ens x Mon x Lat x Lon]
+        
+        
+        lon_out    = ds_eof_raw[v].lon
+        lat_out    = ds_eof_raw[v].lat
+        
+        
+    
+            
+            
+        # Compute pointwise correction
+        correction_diff = monvarfp - eofs_std
+        
+        # Prepare for output -----
+        corcoords     = dict(ens=ds_std[0].ens,mon=np.arange(1,13,1),lat=lat_out,lon=lon_out)
+        eofcoords     = dict(mode=dseof.mode,ens=ds_std[0].ens,mon=np.arange(1,13,1),lat=lat_out,lon=lon_out)
+        
+        da_correction = xr.DataArray(correction_diff,coords=corcoords,dims=corcoords,name="correction_factor")
+        da_eofs_filt  = xr.DataArray(eofs_filtered,coords=eofcoords,dims=eofcoords  ,name=vnames[v])
+        
+        ds_out        = xr.merge([da_correction,da_eofs_filt])
+        edict         = proc.make_encoding_dict(ds_out)
+        
+        # Save for all ensemble members
+        savename       = proc.addstrtoext(ncnames[v],"_corrected",adjust=-1)
+        ds_out.to_netcdf(savename,encoding=edict)
+        
+        savename_emean = proc.addstrtoext(savename,"_EnsAvg",adjust=-1)
+        ds_out_ensavg  = ds_out.mean('ens')
+        ds_out_ensavg.to_netcdf(savename_emean,encoding=edict)
+        
+        # if crop_sm:
+            
+            
+            
+        #     print("Cropping to region %s" % (regstr_crop))
+        #     ds_out = proc.lon360to180_xr(ds_out)
+            
+        #     ds_out_reg = proc.sel_region_xr(ds_out,bbox_crop)
+        #     savename_reg = proc.addstrtoext(ncnames[v],"_corrected",adjust=-1).replace(regstr,regstr_crop)
+        #     ds_out_reg.to_netcdf(savename_reg,encoding=edict)
+            
+        #     savename_emean = proc.addstrtoext(savename_reg,"_EnsAvg",adjust=-1)
+        #     ds_out_reg_ensavg  = ds_out_reg.mean('ens')
+        #     ds_out_reg_ensavg.to_netcdf(savename_emean,encoding=edict)
+        #     print("Saved Ens Avg. Cropped Output to %s" % savename_emean)
 
 # Check values
 if debug:
