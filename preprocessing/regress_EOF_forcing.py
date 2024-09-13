3300fcf43c7ccdf12b1940633073aa73c92f698c
Glenn Liu <glenn.y.liu@gmail.com>
2024-09-13 15:47:19 -0400

    Updated regress_EOF_forcing
    = Added option to concatenate ensemble members

--- preprocessing/regress_EOF_forcing.py
+++ preprocessing/regress_EOF_forcing.py
@@ -42,51 +42,87 @@ import xarray as xr
 import time
 from tqdm import tqdm
 
+# ----------------------------------
+#%% Import custom modules and paths
+# ----------------------------------
+
+# Indicate the Machine!
+machine = "stormtrack"
+
+# First Load the Parameter File
+sys.path.append("../")
+import reemergence_params as rparams
+
+# Paths and Load Modules
+pathdict   = rparams.machine_paths[machine]
+
+sys.path.append(pathdict['amvpath'])
+sys.path.append(pathdict['scmpath'])
+from amv import proc,viz
+import scm
+import amv.loaders as dl
+import yo_box as ybx
+
+# Set needed paths
+figpath     = pathdict['figpath']
+proc.makedir(figpath)
+input_path  = pathdict['input_path']
+output_path = pathdict['output_path']
+procpath    = pathdict['procpath']
+
+
+rawpath1 = pathdict['raw_path']
+dpath    = input_path + "damping/"
+mldpath  = input_path + "mld/"
+fpath    = input_path + 'forcing/'
+
+rawpath = fpath
+
+
+
 #%% Import modules
 
-stormtrack      = 1
+# stormtrack      = 1
 
-if stormtrack == 0:
+# if stormtrack == 0:
     
-    projpath    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/"
-    datpath     = projpath + '01_Data/model_output/'
-    outpathdat  = datpath + '/proc/'
-    figpath     = projpath + "02_Figures/20240207"
+#     projpath    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/"
+#     datpath     = projpath + '01_Data/model_output/'
+#     outpathdat  = datpath + '/proc/'
+#     figpath     = projpath + "02_Figures/20240207"
     
-    lipath      = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/landicemask_enssum.npy"
-    rawpath     = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/CESM1/NATL_proc/"
+#     lipath      = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/01_Data/landicemask_enssum.npy"
+#     rawpath     = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/CESM1/NATL_proc/"
     
-    sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/")
-    sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/")
+#     sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/")
+#     sys.path.append("/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/")
     
-    # Path of model input
-    outpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/model_input/forcing/"
+#     # Path of model input
+#     outpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/model_input/forcing/"
 
-elif stormtrack == 1:
+# elif stormtrack == 1:
     
-    datpath     = ""#"/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/model_output/"
-    rawpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/model_input/forcing/"#/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/model_input/"
-    outpathdat  = datpath + '/proc/'
+#     datpath     = ""#"/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/model_output/"
+#     rawpath     = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/model_input/forcing/"#/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/Model_Data/model_input/"
+#     outpathdat  = datpath + '/proc/'
     
-    # Forcing Output Path
-    fpath       = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/model_input/forcing/"
+#     # Forcing Output Path
+#     fpath       = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/proc/model_input/forcing/"
     
-    sys.path.append("/home/glliu/00_Scripts/01_Projects/00_Commons/")
-    sys.path.append("/home/glliu/00_Scripts/01_Projects/01_AMV/02_stochmod/stochmod/model/")
+#     sys.path.append("/home/glliu/00_Scripts/01_Projects/00_Commons/")
+#     sys.path.append("/home/glliu/00_Scripts/01_Projects/01_AMV/02_stochmod/stochmod/model/")
 
 from amv import proc,viz
 import scm
 import tbx
 
-if stormtrack == 0: # Haven't added a figpath yet for stortmrack
-    proc.makedir(figpath)
 
 #%% User Edits
 
-Eprime = True # Set to True to Load Eprime as LHFLX 
+Eprime       = True # Set to True to Load Eprime as LHFLX 
+concat_ens   = True
 
-
-dataset_name = "cesm1le_htr_5degbilinear"
+dataset_name = "CESM1_HTR"#"cesm1le_htr_5degbilinear"
 
 
 if dataset_name == "CESM1_HTR":
@@ -103,13 +139,13 @@ if dataset_name == "CESM1_HTR":
     # Path to Eprime processed by calc_Fprime_lens
     rawpath3 = rawpath1
     ncstr3   = "CESM1_HTR_FULL_Eprime_timeseries_LHFLXnomasklag1_nroll0_NAtl.nc"
-
+    
+    # Path to 
+    
     # EOF Information
     dampstr    = "nomasklag1"
     rollstr    = "nroll0"
-    eofname    = "%sEOF_Monthly_NAO_EAP_Fprime_%s_%s_NAtl.nc" % (rawpath1,dampstr,rollstr)
-    
-    
+    eofname    = "%sCESM1_HTR_EOF_Monthly_NAO_EAP_Fprime_%s_%s_NAtl.nc" % (rawpath1,dampstr,rollstr)
     
 elif dataset_name == "cesm1le_htr_5degbilinear":
     
@@ -134,6 +170,8 @@ elif dataset_name == "cesm1le_htr_5degbilinear":
     dampstr_qnet    = "cesm1le5degqnet"  
     eofname         = "%s%s_EOF_Monthly_NAO_EAP_Fprime_%s_%s_%s.nc" % (rawpath1,dataset_name,dampstr_qnet,rollstr,regstr)
     
+if concat_ens:
+    eofname = proc.addstrtoext(eofname,"_concatEns",adjust=-1)
     
 # Set Constants
 omega = 7.2921e-5 # rad/sec
@@ -148,7 +186,6 @@ mons3 = proc.get_monstr()#('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep'
 #     - 
 
 varnames_in = ["LHFLX","PRECTOT"]
-
 debug       = True  # Set to True to visualize for debugging
 
 
@@ -169,7 +206,7 @@ dsp     = xr.open_dataset(rawpath2+ncprec).load()
 vn      = "LHFLX"
 if Eprime:
     print("Loading Eprime as LHFLX")
-    dslhf   = xr.open_dataset(ncstr3).load()
+    dslhf   = xr.open_dataset(rawpath3+ncstr3).load()
 else:
     dslhf   = xr.open_dataset(rawpath1+ ncstr1 % vn).load()
 if 'ensemble' in dslhf.dims:
@@ -178,7 +215,10 @@ if 'ensemble' in dslhf.dims:
 # Loop and preprocess
 ds_in     = [dsp,dslhf]
 ds_in     = [proc.fix_febstart(ds) for ds in ds_in]
-ds_vnames = ["PRECTOT","Eprime"]
+ds_vnames = ["PRECTOT","LHFLX"]
+
+
+#%% Do Preprocessing
 
 # Remove seasonal cycle
 ds_anom  = [proc.xrdeseason(ds) for ds in ds_in]
@@ -200,7 +240,7 @@ pcstd = pcs / pcs.std('yr')
 #%% Perform Regressions
 
 # Make into a simple function
-def regress_pc_monens(ds,pcstd,vname,return_ds=True):
+def regress_pc_monens(ds,pcstd,vname,return_ds=True,concat_ens=False):
     
     # Given standardized PCs (mon, ens)
     # Regress the corresponding variable [vname] in ds (ens x yr x mon x lat x lon)
@@ -214,7 +254,15 @@ def regress_pc_monens(ds,pcstd,vname,return_ds=True):
     
     # Combine spatial dimensions and loop
     invar        = ds.values
-    invar        = invar.reshape(nens,nyr,nmon,npts)
+    if concat_ens:
+        print("Reshaping by combining Ens and Year Dimensions")
+        nyr   = nyr * nens#int(ntime/12)
+        nens  = 1
+        invar = invar.reshape(1,nyr,nmon,npts)
+        nens,ntime,_,npts = invar.shape
+    else:
+        invar        = invar.reshape(nens,nyr,nmon,npts)
+        
     regr_pattern = np.zeros((nens,nlat*nlon,nmon,nmode)) * np.nan # [ens, space, month, mode]
     for e in tqdm(range(nens)):
         for im in range(nmon):
@@ -234,7 +282,7 @@ def regress_pc_monens(ds,pcstd,vname,return_ds=True):
     regr_pattern = regr_pattern.reshape(nens,nlat,nlon,nmon,nmode)
     
     if return_ds:
-        cout         = dict(ens=ds.ens,lat=ds.lat,lon=ds.lon,mon=np.arange(1,13,1),mode=pcstd.mode)
+        cout         = dict(ens=np.arange(1,1+nens),lat=ds.lat,lon=ds.lon,mon=np.arange(1,13,1),mode=pcstd.mode)
         da_out       = xr.DataArray(regr_pattern,coords=cout,dims=cout,name=vname)
         da_out       = da_out.transpose('mode','ens','mon','lat','lon')
         return da_out
@@ -250,9 +298,8 @@ for v in range(nvars):
     vname  = ds_vnames[v] 
     ds     = ds_dt[v][vname]
     
-    da_out = regress_pc_monens(ds,pcstd,vname)
+    da_out = regress_pc_monens(ds,pcstd,vname,concat_ens=concat_ens)
     regression_maps.append(da_out)
-
 
 #%% Save the Output
 
@@ -268,12 +315,16 @@ for v in range(nvars):
         outvname = "Eprime"
     else:
         outvname = ds_vnames[v]
-        
+    
     # Set the save name
     if dataset_name == "CESM1_HTR":
         savename = "%sCESM1_HTR_FULL_%s_EOF_%s_%s_NAtl.nc" % (fpath,outvname,dampstr,rollstr) # ('mode','ens','mon','lat','lon')
     else:
         savename = "%s%s_%s_EOF_%s_%s_%s.nc" % (fpath,dataset_name,outvname,dampstr,rollstr,regstr)
+      
+    if concat_ens:
+        savename = proc.addstrtoext(savename,"_concatEns",adjust=-1)
+    #print(savename)
     
     # Save the output (all ensemble members)
     da_out.to_netcdf(savename,encoding=edict)
@@ -284,5 +335,7 @@ for v in range(nvars):
     da_out_ensavg.to_netcdf(savename_ea,encoding=edict)
     print("Saved %s to %s!" % (vname,savename_ea))
     
-    
+#%% Test Visualization
+
+
 
