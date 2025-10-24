92702cec16b1663dce03f04b39e317e7f956058b
Glenn Liu <glenn.y.liu@gmail.com>
2025-10-23 15:01:29 -1000

    Run just for SPGNE

--- stochastic_model/run_SSS_basinwide.py
+++ stochastic_model/run_SSS_basinwide.py
@@ -96,11 +96,6 @@ expparams   = {
 #%%
 
 
-"""
-Paste Experiment Parameters Below (see basinwide_experiment_params.py)
-
-"""
-
 
 expname     = "SST_ORAS5_avg_GMSST_EOFmon_usevar_NoRem_NATL"
 expparams   = {
@@ -135,6 +130,45 @@ expparams   = {
     "correct_Qek"       : False, # Set to True if correction factor to Qek was calculated
     "convert_Qek"       : False, # Set to True if Qek is in W/m2 (True for old SST forcing...) False if in psu/sec or degC/sec (for new scripts)
     }
+
+
+
+
+
+expname     = "SST_ORAS5_avg_GMSST_EOFmon_usevar_NoRem_SPGNE"
+expparams   = {
+    'varname'           : "SST",
+    'bbox_sim'          : [-40,-15,52,62],
+    'nyrs'              : 1000,
+    'runids'            : ["run%02i" % i for i in np.arange(0,10,1)],
+    'runid_path'        : "SST_ORAS5_avg_EOF", # If not None, load a runid from another directory
+    'Fprime'            : "ERA5_Fprime_QNET_timeseries_QNETgmsstMON_nroll0_NAtl_EOFFilt090_corrected_usevar.nc",
+    'PRECTOT'           : None,
+    'LHFLX'             : None,
+    'h'                 : "MIMOC_regridERA5_h_pilot.nc",
+    'lbd_d'             : "ORAS5_avg_MIMOC_corr_d_TEMP_detrendGMSSTmon_lagmax3_interp1_ceil0_imshift1_dtdepth1_1979to2024_regridERA5.nc",
+    'Sbar'              : None,
+    'beta'              : None, # If None, just compute entrainment damping
+    'kprev'             : "MIMOC_regridERA5_kprev_pilot.nc",
+    'lbd_a'             : "ERA5_qnet_damping_AConly_detrendGMSSTmon.nc", # NEEDS TO BE CONVERTED TO 1/Mon !!!
+    'Qek'               : None, # Now in degC/sec
+    'convert_Fprime'    : True,
+    'convert_lbd_a'     : True, 
+    'convert_PRECTOT'   : False,
+    'convert_LHFLX'     : False,
+    'froll'             : 0,
+    'mroll'             : 0,
+    'droll'             : 0,
+    'halfmode'          : False,
+    "entrain"           : True,
+    "eof_forcing"       : True, # CHECK THIS
+    "Td_corr"           : False, # Set to True if lbd_d is provided as a correlation, rather than 1/months
+    "lbd_e"             : None, # Relevant for SSS
+    "Tforce"            : None, # Relevant for SSS
+    "correct_Qek"       : False, # Set to True if correction factor to Qek was calculated
+    "convert_Qek"       : False, # Set to True if Qek is in W/m2 (True for old SST forcing...) False if in psu/sec or degC/sec (for new scripts)
+    }
+
 
 
 
@@ -309,7 +343,7 @@ for nr in range(nruns):
     
     # Generate or reload white noise
     if len(glob.glob(noisefile)) > 0:
-        print("\t\tWhite file has been found! Loading...")
+        print("\t\tWhite Noise file has been found! Loading...")
         wn = np.load(noisefile)
     else:
         print("\t\tGenerating new white noise file: %s" % noisefile)
